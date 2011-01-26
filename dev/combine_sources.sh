7f6abe9ffb2d82d99094541c7f8d514c7c37d687
Takashi Okamoto <mud@media.mit.edu>
2011-01-26 13:06:42 -0500

    Added flowplayer to combine script. Added id to div container for flash fallback, to remove unspecified error thrown by IE8.

--- dev/combine_sources.sh
+++ dev/combine_sources.sh
@@ -4,6 +4,7 @@ cat src/_begin.js > combined.js
 
 cat src/video.js >> combined.js
 cat src/html5.js >> combined.js
+cat flowplayer/video-js.flowplayer.js >> combined.js
 cat src/behaviors.js >> combined.js
 cat src/lib.js >> combined.js
 cat src/video-js.jquery.js >> combined.js
