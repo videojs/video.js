90ac46a585f99aa6c470fb62af5c7b453f464c75
Dan Connor <danconn@danconnor.com>
2012-10-10 01:57:02 -0700

    adding cookie setter, getter, eraser

--- src/lib.js
+++ src/lib.js
@@ -33,6 +33,30 @@ _V_.extend({
   ua: navigator.userAgent,
   support: {},
 
+  setCookie: function(name,value,days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    } else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+  },
+
+  getCookie: function(name) {
+    var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+  },
+
+  eraseCookie: function(name) {
+    setCookie(name,"",-1);
+  },
+
   each: function(arr, fn){
     if (!arr || arr.length === 0) { return; }
     for (var i=0,j=arr.length; i<j; i++) {
