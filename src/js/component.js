b6ea80b67ef1b99f0fc00405735cee767af8fb16
Steve Heffernan <steve@zencoder.com>
2013-01-21 17:10:54 -0800

    Updated component children adding to allow no name, id, or el

--- src/js/component.js
+++ src/js/component.js
@@ -300,21 +300,25 @@ vjs.Component.prototype.addChild = function(child, options){
     component = child;
   }
 
-  componentName = component.name();
-  componentId = component.id();
-
   this.children_.push(component);
 
-  if (componentId) {
-    this.childIndex_[componentId] = component;
+  if (typeof component.id === 'function') {
+    this.childIndex_[component.id()] = component;
   }
+
+  // If a name wasn't used to create the component, check if we can use the
+  // name function of the component
+  componentName = componentName || (component.name && component.name());
 
   if (componentName) {
     this.childNameIndex_[componentName] = component;
   }
 
   // Add the UI object's element to the container div (box)
-  this.el_.appendChild(component.el());
+  // Having an element is not required
+  if (typeof component.el === 'function' && component.el()) {
+    this.el_.appendChild(component.el());
+  }
 
   // Return so it can stored on parent object if desired.
   return component;
