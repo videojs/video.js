e28505295b0abb22e6917594fcb87303a4342666
corwin.tomkinson <corwin.tomkinson@axonn.co.uk>
2013-03-07 12:05:22 +0000

    add isPlainObject method copied from JQuery

--- src/js/lib.js
+++ src/js/lib.js
@@ -142,6 +142,40 @@ vjs.obj.type = function (obj) {
 };
 
 /**
+ * Checks whether an object is an plain object and not a DOM object or window
+ * @param  {Object} obj Object to check type of
+ * @return {Boolean}    Whether the object is plain
+ */
+vjs.obj.isPlainObject = function (obj) {
+    // Must be an Object.
+    // Because of IE, we also have to check the presence of the constructor property.
+    // Make sure that DOM nodes and window objects don't pass through, as well
+    if (!obj || vjs.obj.type(obj) !== "object" || obj.nodeType || vjs.obj.isWindow(obj)) {
+        return false;
+    }
+
+    try {
+        // Not own constructor property must be Object
+        if ( obj.constructor &&
+            !vjs.obj.hasOwnProperty.call(obj, "constructor") &&
+            !vjs.obj.hasOwnProperty.call(obj.constructor.prototype, "isPrototypeOf")) {
+            return false;
+        }
+    } catch ( e ) {
+        // IE8,9 Will throw exceptions on certain host objects
+        return false;
+    }
+
+    // Own properties are enumerated firstly, so to speed up,
+    // if last one is own, then all properties are own.
+
+    var key;
+    for ( key in obj ) {}
+
+    return key === undefined || vjs.obj.hasOwnProperty.call(obj, key);
+};
+
+/**
  * Bind (a.k.a proxy or Context). A simple method for changing the context of a function
    It also stores a unique id on the function so it can be easily removed from events
  * @param  {*}   context The object to bind as scope
