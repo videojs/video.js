0ba71666363d5a519e44b44e974d1bf0f6a40b18
David LaPalomento <dlapalomento@gmail.com>
2013-03-07 20:57:52 -0500

    Fixes #367 If you are quick, you could get videojs to wrap the HTML video element twice on iOS. The video element has to be recreated after moving on iOS but we weren't re-associating it with the player object. When autoSetup swung through, it would re-initialize the video element and create a player within a player.

--- test/unit/media.html5.js
+++ test/unit/media.html5.js
@@ -16,3 +16,22 @@ test('should detect whether the volume can be changed', function(){
   ok(!vjs.Html5.canControlVolume());
   vjs.TEST_VID = testVid;
 });
+
+test('should re-link the player if the tech is moved', function(){
+  var player, tech, el;
+  el = document.createElement('div');
+  el.innerHTML = '<div />';
+  player = {
+    id: function(){ return 'id'; },
+    el: function(){ return el; },
+    options_: {},
+    ready: function(){}
+  };
+  tech = new vjs.Html5(player, {});
+  tech.features = {
+    movingMediaElementInDOM: false
+  };
+  tech.createEl();
+
+  strictEqual(player, tech.el().player);
+});
