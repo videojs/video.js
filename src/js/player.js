7455131f5378e31f7b9da922d6da6a98c5ab44d3
Mykhailo Stadnyk <mikhus@gmail.com>
2013-07-29 22:31:00 +0300

    buffered() minor fixes due to request

--- src/js/player.js
+++ src/js/player.js
@@ -544,12 +544,12 @@ vjs.Player.prototype.remainingTime = function(){
 vjs.Player.prototype.buffered = function(){
   var buffered = this.techGet('buffered'),
       start = 0,
-      buflen = buffered.length,
+      buflast = buffered.length - 1,
       // Default end to 0 and store in values
       end = this.cache_.bufferEnd = this.cache_.bufferEnd || 0;
 
-  if (buffered && buflen > 0 && buffered.end( buflen - 1) !== end) {
-    end = buffered.end( buflen - 1);
+  if (buffered && buflast >= 0 && buffered.end(buflast) !== end) {
+    end = buffered.end(buflast);
     // Storing values allows them be overridden by setBufferedFromProgress
     this.cache_.bufferEnd = end;
   }
