75b2171051eae4da454d3ecac30b360a8d5ca57a
David LaPalomento <dlapalomento@gmail.com>
2013-12-02 15:34:00 -0800

    Close GH-850: Use qunitjs through npm.

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
       },
       tests: {
         src: ['build/files/combined.video.js', 'build/compiler/goog.base.js', 'src/js/exports.js', 'test/unit/*.js', '!test/unit/api.js'],
-        externs: ['src/js/media/flash.externs.js', 'test/qunit/qunit-externs.js'],
+        externs: ['src/js/media/flash.externs.js', 'test/qunit-externs.js'],
         dest: 'build/files/test.minified.video.js'
       }
     },
