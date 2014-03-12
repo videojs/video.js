26f6f4b0ea17eb7da201a19f3174d2949e40fe21
Stephan Hesse <tchakabam@gmail.com>
2014-03-13 00:46:00 +0100

    Enough to check for this.tech != null

--- src/js/player.js
+++ src/js/player.js
@@ -1275,7 +1275,7 @@ vjs.Player.prototype.userActive = function(bool){
         // When this gets resolved in ALL browsers it can be removed
         // https://code.google.com/p/chromium/issues/detail?id=103041
         
-        if(typeof(this.tech) != 'undefined') {
+        if(this.tech) {
           this.tech.one('mousemove', function(e){
             e.stopPropagation();
             e.preventDefault();
