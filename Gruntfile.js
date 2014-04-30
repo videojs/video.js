321b0055df0483d7460212393a052520d0e391af
Gary Katsevman <git@gkatsev.com>
2014-04-30 19:19:24 -0400

    remove options from grunt karma. Non-verbose grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -135,9 +135,6 @@ module.exports = function(grunt) {
       }
     },
     karma: {
-      options: {
-        configFile: 'test/localkarma.conf.js'
-      },
       saucelabs: {
         configFile: 'test/karma.conf.js'
       },
