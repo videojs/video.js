5ff4bfb0eeef71894ef43cf3a2651fe21df4b39f
Steve Heffernan <steve@zencoder.com>
2014-05-14 18:18:14 -0700

    Changed file name to fullscreen-api.js to be less generic.

--- build/source-loader.js
+++ build/source-loader.js
@@ -25,7 +25,7 @@ var sourceFiles = [
   "src/js/slider.js",
   "src/js/menu.js",
   "src/js/media-error.js",
-  "src/js/fullscreen.js",
+  "src/js/fullscreen-api.js",
   "src/js/player.js",
   "src/js/control-bar/control-bar.js",
   "src/js/control-bar/live-display.js",
