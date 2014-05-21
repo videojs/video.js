c9c4dcf83355baf1461021c9903ce3eb8ab5c7b8
Steve Heffernan <steve@zencoder.com>
2014-05-21 16:46:18 -0700

    Added test for cue parsing, including tests for #877

--- test/unit/tracks.js
+++ test/unit/tracks.js
@@ -14,3 +14,15 @@ test('should place title list item into ul', function() {
 
   player.dispose();
 });
+
+test('cue time parsing', function() {
+  var parse = vjs.TextTrack.prototype.parseCueTime;
+
+  equal(parse('11:11'), 671, 'Only minutes and seconds (11:11)');
+  equal(parse('11:11:11'), 40271, 'Hours, minutes, seconds (11:11:11)');
+  equal(parse('11:11:11.111'), 40271.111, 'Hours, minutes, seconds, decimals (11:11:11.111)');
+
+  // Uncommment to test a fix for #877
+  // equal(parse('11:11 line:90%'), 671, 'minutes, seconds with flags');
+  // equal(parse('11:11:11 line:90%'), 40271, 'hours, minutes, seconds with flags');
+});
