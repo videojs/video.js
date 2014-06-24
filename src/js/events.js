0ee79afdfe7d1f875195eb353700c0e6f38c30b4
Erick Acuna <eacuna@brightcove.com>
2014-06-24 12:43:18 -0400

    Fixed issue number 1298 component.trigger does not pass event object #1298

--- src/js/events.js
+++ src/js/events.js
@@ -265,7 +265,9 @@ vjs.fixEvent = function(event) {
 /**
  * Trigger an event for an element
  * @param  {Element|Object} elem  Element to trigger an event on
- * @param  {String} event Type of event to trigger
+ * @param  {Object}         event either a string (the event type) 
+ *                          to trigger or an event object with a 
+ *                          type attribute.
  * @private
  */
 vjs.trigger = function(elem, event) {
