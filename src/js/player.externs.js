612c1d10028d4208c3f095d4063a326d61d1fd3f
Matt Osborn <matthew.osborn@theguardian.com>
2014-07-01 10:47:39 +0100

    xRevert "adding techName and loadTech to externs"
    This reverts commit 1272601954b4931ffb70c096f7fa183aebefe1fe.

--- src/js/player.externs.js
+++ src/js/player.externs.js
@@ -81,9 +81,3 @@ videojs.Player.prototype.userActive = function(){};
  * Native controls
  */
 videojs.Player.prototype.usingNativeControls = function(){};
-
-/**
- * Tech
- */
-videojs.Player.prototype.techName = function(){};
-videojs.Player.prototype.loadTech = function(){};
