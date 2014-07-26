2db7efdeea6ae443c476e6f454d9cb027b3e8119
jackunion <verteletskydmitry@gmail.com>
2014-07-26 21:08:45 +0400

    fixes #895
    Moved event.preventDefault() to 'touchend' listener

--- src/js/media/media.js
+++ src/js/media/media.js
@@ -87,8 +87,6 @@ vjs.MediaTechController.prototype.addControlsListeners = function(){
   // so we'll check if the controls were already showing before reporting user
   // activity
   this.on('touchstart', function(event) {
-    // Stop the mouse events from also happening
-    event.preventDefault();
     userWasActive = this.player_.userActive();
   });
 
@@ -97,6 +95,11 @@ vjs.MediaTechController.prototype.addControlsListeners = function(){
       this.player().reportUserActivity();
     }
   });
+  
+  this.on('touchend', function(event) {
+    // Stop the mouse events from also happening
+    event.preventDefault();
+  })
 
   // Turn on component tap events
   this.emitTapEvents();
