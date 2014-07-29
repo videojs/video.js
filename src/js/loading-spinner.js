1625152131d385041854781d493be2f584312c19
Steve Heffernan <git@heff.me>
2014-07-29 11:48:23 -0700

    Split up seeking and waiting styles. Updated loading spinner to use them.

--- src/js/loading-spinner.js
+++ src/js/loading-spinner.js
@@ -12,23 +12,25 @@ vjs.LoadingSpinner = vjs.Component.extend({
   init: function(player, options){
     vjs.Component.call(this, player, options);
 
-    player.on('canplay', vjs.bind(this, this.hide));
-    player.on('canplaythrough', vjs.bind(this, this.hide));
-    player.on('playing', vjs.bind(this, this.hide));
-    player.on('seeking', vjs.bind(this, this.show));
+    // MOVING DISPLAY HANDLING TO CSS
+
+    // player.on('canplay', vjs.bind(this, this.hide));
+    // player.on('canplaythrough', vjs.bind(this, this.hide));
+    // player.on('playing', vjs.bind(this, this.hide));
+    // player.on('seeking', vjs.bind(this, this.show));
 
     // in some browsers seeking does not trigger the 'playing' event,
     // so we also need to trap 'seeked' if we are going to set a
     // 'seeking' event
-    player.on('seeked', vjs.bind(this, this.hide));
+    // player.on('seeked', vjs.bind(this, this.hide));
 
-    player.on('ended', vjs.bind(this, this.hide));
+    // player.on('ended', vjs.bind(this, this.hide));
 
     // Not showing spinner on stalled any more. Browsers may stall and then not trigger any events that would remove the spinner.
     // Checked in Chrome 16 and Safari 5.1.2. http://help.videojs.com/discussions/problems/883-why-is-the-download-progress-showing
     // player.on('stalled', vjs.bind(this, this.show));
 
-    player.on('waiting', vjs.bind(this, this.show));
+    // player.on('waiting', vjs.bind(this, this.show));
   }
 });
 
