6cc58e77290b261b9fe365f73682ff36abd8a411
Ryo Chikazawa <chikathreesix@gmail.com>
2014-09-18 11:30:23 +0800

    Use PlayerTest instead of creating Player element from string

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,17 +1,10 @@
 module('Setup');
 
 test('should set options from data-setup even if autoSetup is not called before initialisation', function(){
-  var fixture = document.getElementById('qunit-fixture');
+  var el = PlayerTest.makeTag();
+  el.setAttribute('data-setup', '{"controls": true, "autoplay": false, "preload": "auto"}');
 
-  var html = '<video id="example_1" class="video-js" data-setup="{&quot;controls&quot;: true, &quot;autoplay&quot;: false, &quot;preload&quot;: &quot;auto&quot; }">';
-      html += '<source src="http://google.com" type="video/mp4">';
-      html += '<source src="http://google.com" type="video/webm">';
-      html += '</video>';
-
-  fixture.innerHTML += html;
-
-  var tag = document.getElementById('example_1');
-  var player = vjs('example_1');
+  var player = PlayerTest.makePlayer({}, el);
 
   ok(player.options_['controls'] === true);
   ok(player.options_['autoplay'] === false);
