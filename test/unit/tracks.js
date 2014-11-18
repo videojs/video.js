93e27a4d6d8f4e76ebc0d5230609f6eeac689002
Gary Katsevman <git@gkatsev.com>
2014-11-18 18:26:22 -0500

    A whole bunch of tests

--- test/unit/tracks.js
+++ test/unit/tracks.js
@@ -396,3 +396,263 @@ test('if native text tracks are not supported, create a texttrackdisplay', funct
   vjs.IS_FIREFOX = oldIsFirefox;
   window['videojs']['TextTrackDisplay'] = oldTextTrackDisplay;
 });
+
+test('Player track methods call the tech', function() {
+  var player,
+      calls = 0;
+
+  player = PlayerTest.makePlayer();
+
+  player.tech.textTracks = function() {
+    calls++;
+  };
+  player.tech.addTextTrack = function() {
+    calls++;
+  };
+
+  player.addTextTrack();
+  player.textTracks();
+
+  equal(calls, 2, 'both textTrack and addTextTrack defer to the tech')
+});
+
+test('TextTrackDisplay initializes tracks on player ready', function() {
+  var calls = 0,
+      ttd = new vjs.TextTrackDisplay({
+        addTextTracks: function() {
+          calls--;
+        },
+        getChild: function() {
+          calls--;
+        },
+        ready: function() {
+          calls++;
+        }
+      }, {});
+
+  equal(calls, 1, 'only a player.ready call was made');
+});
+
+test('TextTrackDisplay adds tracks and updates children', function() {
+  var add = 0,
+      get = 0,
+      update = 0,
+      player = {
+        addTextTracks: vjs.Player.prototype.addTextTracks,
+        addTextTrack: function() {
+          add++;
+        },
+        getChild: function(child) {
+          get++;
+          return {
+            getChild: function() {
+              get++;
+              return {
+                update: function() {
+                  update++;
+                }
+              }
+            }
+          }
+        },
+        ready: function(cb) {
+          cb.call(this);
+        }
+      },
+      options = {
+        tracks: [{
+          kind: 'captions',
+          label: 'en',
+          language: 'English',
+          src: 'en.vtt'
+        }, {
+          kind: 'captions',
+          label: 'es',
+          language: 'Spanish',
+          src: 'es.vtt'
+        }]
+      },
+      ttd;
+
+  player.player_ = player;
+  player.options_ = options;
+
+  ttd = new vjs.TextTrackDisplay(player, options);
+
+  equal(add, 2, 'calls to addTextTrack');
+  equal(get, 4, 'four children were request: controlBar and its children subtitles, captions, and chapters buttons');
+  equal(update, 3, 'each button should have been updated');
+});
+
+test('html5 tech supports native text tracks if the video supports it', function() {
+  var oldTestVid = vjs.TEST_VID,
+      player,
+      options,
+      html;
+
+  vjs.TEST_VID = {
+    textTracks: []
+  };
+
+  player = {
+    controls: Function.prototype,
+    ready: Function.prototype,
+    options: Function.prototype,
+    addChild: Function.prototype,
+    id: Function.prototype,
+    el: function() {
+      return {
+        insertBefore: Function.prototype,
+        appendChild: Function.prototype
+      };
+    }
+  };
+  player.player_ = player;
+  player.options_ = options = {};
+
+  html = new vjs.Html5(player, options);
+
+  ok(html['featuresNativeTracks'], 'if textTracks are available on video element, native text tracks are supported');
+
+  vjs.TEST_VID = oldTestVid;
+});
+
+test('html5 tech supports native text tracks if the video supports it, unless mode is a number', function() {
+  var oldTestVid = vjs.TEST_VID,
+      player,
+      options,
+      html;
+
+  vjs.TEST_VID = {
+    textTracks: []
+  };
+
+  player = {
+    controls: Function.prototype,
+    ready: Function.prototype,
+    options: Function.prototype,
+    addChild: Function.prototype,
+    id: Function.prototype,
+    el: function() {
+      return {
+        insertBefore: Function.prototype,
+        appendChild: Function.prototype
+      };
+    }
+  };
+  player.player_ = player;
+  player.options_ = options = {
+    parentEl: {
+      firstChild: {
+        textTracks: [{
+          mode: 0
+        }]
+      }
+    }
+  };
+
+  html = new vjs.Html5(player, options);
+
+  ok(!html['featuresNativeTracks'], 'native text tracks are not supported if mode is a number');
+
+  vjs.TEST_VID = oldTestVid;
+});
+
+test('html5 tech supports native text tracks if the video supports it, unless it is firefox', function() {
+  var oldTestVid = vjs.TEST_VID,
+      oldIsFirefox = vjs.IS_FIREFOX,
+      player,
+      options,
+      html;
+
+  vjs.TEST_VID = {
+    textTracks: []
+  };
+
+  player = {
+    controls: Function.prototype,
+    ready: Function.prototype,
+    options: Function.prototype,
+    addChild: Function.prototype,
+    id: Function.prototype,
+    el: function() {
+      return {
+        insertBefore: Function.prototype,
+        appendChild: Function.prototype
+      };
+    }
+  };
+  player.player_ = player;
+  player.options_ = options = {};
+
+  vjs.IS_FIREFOX = true;
+  html = new vjs.Html5(player, options);
+
+  ok(!html['featuresNativeTracks'], 'if textTracks are available on video element, native text tracks are supported');
+
+  vjs.TEST_VID = oldTestVid;
+  vjs.IS_FIREFOX = oldIsFirefox;
+});
+
+test('in html5, if native text tracks are not supported, create a texttrackdisplay', function() {
+  var oldTestVid = vjs.TEST_VID,
+      oldIsFirefox = vjs.IS_FIREFOX,
+      oldTextTrackDisplay = vjs.TextTrackDisplay,
+      called = false,
+      player,
+      options,
+      html;
+
+  vjs.TEST_VID = {
+    textTracks: []
+  };
+
+  player = {
+    controls: Function.prototype,
+    ready: Function.prototype,
+    options: Function.prototype,
+    addChild: Function.prototype,
+    id: Function.prototype,
+    el: function() {
+      return {
+        insertBefore: Function.prototype,
+        appendChild: Function.prototype
+      };
+    }
+  };
+  player.player_ = player;
+  player.options_ = options = {};
+
+  vjs.TextTrackDisplay = function() {
+    called = true;
+  };
+
+  vjs.IS_FIREFOX = true;
+  html = new vjs.Html5(player, options);
+
+  ok(called, 'text track display was created');
+
+  vjs.TEST_VID = oldTestVid;
+  vjs.IS_FIREFOX = oldIsFirefox;
+  vjs.TextTrackDisplay = oldTextTrackDisplay;
+});
+
+test('a tech that does not support native text tracks, should create a texttrackdisplay', function() {
+  var tech,
+      oldTextTrackDisplay = vjs.TextTrackDisplay,
+      called = false;
+
+  vjs.TextTrackDisplay = function() {
+    called = true;
+  };
+
+  tech = new vjs.MediaTechController({
+    ready: Function.prototype,
+    addChild: Function.prototype,
+    bufferedPercent: Function.prototype
+  },{}),
+
+  ok(called, 'text track display was created');
+
+  vjs.TextTrackDisplay = oldTextTrackDisplay;
+});
