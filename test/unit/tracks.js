42a8c7a36dbc4cbebf6b2b7b67e4e0c214c011fb
David LaPalomento <dlapalomento@gmail.com>
2014-11-24 19:00:33 -0500

    Remove obsolete cue parsing test Since vtt.js is parsing captions now, we don't need a unit test for captions parsing.

--- test/unit/tracks.js
+++ test/unit/tracks.js
@@ -23,41 +23,6 @@ test('cue time parsing', function() {
   equal(parse('11:11:11.111'), 40271.111, 'Hours, minutes, seconds, decimals (11:11:11.111)');
 });
 
-test('cue parsing', function() {
-  var mockTrack = {
-    cues_: [],
-    reset: function(){ this.cues_ = []; },
-    parseCues: vjs.TextTrack.prototype.parseCues,
-    parseCueTime: vjs.TextTrack.prototype.parseCueTime,
-    trigger: function(){}
-  };
-  var vttHead = 'WEBVTT\n\n';
-
-  var timeWithSpaces = vttHead + '00:00.700 --> 00:04.110\nText line 1';
-  mockTrack.parseCues(timeWithSpaces);
-  equal(mockTrack.cues_[0].startTime, 0.7, 'Cue start time w/ spaces');
-  equal(mockTrack.cues_[0].endTime, 4.11, 'Cue end time w/ spaces');
-  equal(mockTrack.cues_[0].text, 'Text line 1', 'Cue text');
-
-  mockTrack.reset(); // reset mock track
-  var timeWithTabs = vttHead + '00:00.700\t-->\t00:04.110\nText line 1';
-  mockTrack.parseCues(timeWithTabs);
-  equal(mockTrack.cues_[0].startTime, 0.7, 'Cue start time w/ spaces');
-  equal(mockTrack.cues_[0].endTime, 4.11, 'Cue end time w/ spaces');
-
-  mockTrack.reset(); // reset mock track
-  var timeWithMixedWhiteSpace = vttHead + '00:00.700  -->\t 00:04.110\nText line 1';
-  mockTrack.parseCues(timeWithMixedWhiteSpace);
-  equal(mockTrack.cues_[0].startTime, 0.7, 'Cue start time w/ spaces');
-  equal(mockTrack.cues_[0].endTime, 4.11, 'Cue end time w/ spaces');
-
-  mockTrack.reset(); // reset mock track
-  var timeWithFlags = vttHead + '00:00.700  -->\t 00:04.110 line:90% position:26% size:9%\nText line 1';
-  mockTrack.parseCues(timeWithMixedWhiteSpace);
-  equal(mockTrack.cues_[0].startTime, 0.7, 'Cue start time w/ flags');
-  equal(mockTrack.cues_[0].endTime, 4.11, 'Cue end time w/ flags');
-});
-
 test('Player track methods call the tech', function() {
   var player,
       calls = 0;
