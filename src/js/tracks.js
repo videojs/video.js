a8f57f277d322d7dbcfc91e83853702bdeb3fe66
Gary Katsevman <git@gkatsev.com>
2014-11-26 13:40:55 -0500

    Forgot an 's'

--- src/js/tracks.js
+++ src/js/tracks.js
@@ -774,7 +774,7 @@ vjs.CaptionSettingsMenuItem = vjs.TextTrackMenuItem.extend({
 });
 
 vjs.CaptionSettingsMenuItem.prototype.onClick = function() {
-  this.player().getChild('textTrackSetting').show();
+  this.player().getChild('textTrackSettings').show();
 };
 
 /**
