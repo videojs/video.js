015fbecc4a5dda2de5b4fdea0b6f65aefb53d829
Gary Katsevman <git@gkatsev.com>
2014-12-03 16:35:07 -0500

    Add an EventEmitter object

---
+++ src/js/event-emitter.js
@@ -0,0 +1,28 @@
+vjs.EventEmitter = function() {
+}
+
+vjs.EventEmitter.prototype.allowedEvents_ = {
+};
+
+vjs.EventEmitter.prototype.on = function(type, fn) {
+  vjs.on(this, type, fn);
+};
+
+vjs.EventEmitter.prototype.off = function(type, fn) {
+  vjs.off(this, type, fn);
+}
+
+vjs.EventEmitter.prototype.one = function(type, fn) {
+  vjs.one(this, type, fn);
+}
+
+vjs.EventEmitter.prototype.trigger = function(event) {
+  var type = event.type || event;
+
+  if (this.allowedEvents_[type] && this['on' + type]) {
+    this['on' + type]();
+  }
+
+  vjs.trigger(this, event);
+};
+
