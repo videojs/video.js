66f447c2f0e0292c37a23a09e580129a66e4e740
Gary Katsevman <git@gkatsev.com>
2014-12-03 16:53:14 -0500

    TextTrack shim It functions mostly like the native one except cuechange will trigger every timeupdate because there are currently no checks to see if activeCues changed. It assumes the activeCues always change.

---
+++ src/js/tracks/text-track.js
@@ -0,0 +1,160 @@
+/*
+ * https://html.spec.whatwg.org/multipage/embedded-content.html#texttrack
+ *
+ * interface TextTrack : EventTarget {
+ *   readonly attribute TextTrackKind kind;
+ *   readonly attribute DOMString label;
+ *   readonly attribute DOMString language;
+ *
+ *   readonly attribute DOMString id;
+ *   readonly attribute DOMString inBandMetadataTrackDispatchType;
+ *
+ *   attribute TextTrackMode mode;
+ *
+ *   readonly attribute TextTrackCueList? cues;
+ *   readonly attribute TextTrackCueList? activeCues;
+ *
+ *   void addCue(TextTrackCue cue);
+ *   void removeCue(TextTrackCue cue);
+ *
+ *   attribute EventHandler oncuechange;
+ * };
+ */
+
+vjs.TextTrack = function(options) {
+  var tt, id, mode, kind, label, language, cues, activeCues;
+
+  tt = this;
+  if (vjs.IS_IE8) {
+    tt = document.createElement('custom');
+  }
+
+  player = options.player;
+  mode = vjs.TextTrackMode[options.mode] || 'disabled';
+  kind = vjs.TextTrackKind[options.kind] || 'subtitles';
+  label = options.label || '';
+  langauge = options.language || '';
+  id = options.id || '';
+
+  tt.cues_ = [];
+  tt.activeCues_ = [];
+
+  cues = new vjs.TextTrackCueList(this.cues_);
+  activeCues = new vjs.TextTrackCueList(this.activeCues_);
+
+  Object.defineProperty(tt, 'kind', {
+    get: function() {
+      return kind;
+    }
+  });
+
+  Object.defineProperty(tt, 'label', {
+    get: function() {
+      return label;
+    }
+  });
+
+  Object.defineProperty(tt, 'langauge', {
+    get: function() {
+      return language;
+    }
+  });
+
+  Object.defineProperty(tt, 'id', {
+    get: function() {
+      return id;
+    }
+  });
+
+  Object.defineProperty(tt, 'mode', {
+    get: function() {
+      return mode;
+    },
+    set: function(newMode) {
+      if (vjs.TextTrackMode[newMode]) {
+        return;
+      }
+      mode = newMode;
+    }
+  });
+
+  Object.defineProperty(tt, 'cues', {
+    get: function() {
+      return cues;
+    }
+  });
+
+  var changed = false;
+  Object.defineProperty(tt, 'activeCues', {
+    get: function() {
+      var i, l, active, ct, cue;
+
+      if (this.cues.length === 0) {
+        return; // nothing to do
+      }
+
+      ct = player.currentTime();
+      i = 0;
+      l = this.cues.length;
+      active = [];
+      this.activeCues_ = [];
+
+      for (; i < l; i++) {
+        cue = this.cues[i];
+        if (cue.startTime <= ct && cue.endTime >= ct) {
+          active.push(cue);
+        }
+      }
+
+      this.activeCues_ = active;
+      activeCues.setCues_(this.activeCues_);
+
+      setTimeout(vjs.bind(this, function() {
+        this.trigger('cuechange');
+      }), 0);
+
+      return activeCues;
+    }
+  });
+
+  var timeupdateHandler = vjs.bind(this, function() {
+    this.activeCues;
+  });
+  player.on('timeupdate', timeupdateHandler);
+  player.on('dispose', function() {
+    player.off('timeupdate', timeupdateHandler);
+  });
+};
+
+vjs.TextTrack.prototype = vjs.obj.create(vjs.EventEmitter.prototype);
+vjs.TextTrack.prototype.constructor = vjs.TextTrack;
+
+vjs.TextTrack.prototype.allowedEvents_ = {
+  'cuechange': 1
+};
+
+vjs.TextTrack.prototype.addCue = function(cue) {
+  this.cues_.push(cue);
+  this.cues.setCues_(this.cues_);
+};
+
+vjs.TextTrack.prototype.removeCue = function(removeCue) {
+  var i = 0,
+      l = this.cues_.length,
+      cue,
+      removed = false;
+
+  for (; i < l; i++) {
+    cue = this.cues_[i];
+    if (cue === removeCue) {
+      this.cues_.splice(i, 1);
+      removed = true;
+    }
+  }
+
+  if (!removed) {
+    throw new Error('Cue Not Found');
+  }
+
+  this.cues.setCues_(this.cues_);
+};
