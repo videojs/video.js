a0dc9b948743793934ec24598b2813c969b6de6e
Gary Katsevman <git@gkatsev.com>
2014-12-10 15:38:53 -0500

    alias addEventListener/removeEventListener::on/off

--- src/js/event-emitter.js
+++ src/js/event-emitter.js
@@ -7,10 +7,12 @@ vjs.EventEmitter.prototype.allowedEvents_ = {
 vjs.EventEmitter.prototype.on = function(type, fn) {
   vjs.on(this, type, fn);
 };
+vjs.EventEmitter.prototype.addEventListener = vjs.EventEmitter.prototype.on;
 
 vjs.EventEmitter.prototype.off = function(type, fn) {
   vjs.off(this, type, fn);
 };
+vjs.EventEmitter.prototype.removeEventListener = vjs.EventEmitter.prototype.off;
 
 vjs.EventEmitter.prototype.one = function(type, fn) {
   vjs.one(this, type, fn);
@@ -25,4 +27,3 @@ vjs.EventEmitter.prototype.trigger = function(event) {
 
   vjs.trigger(this, event);
 };
-
