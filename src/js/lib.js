ccc486f8aa1bbb4f8deb8a0eafdea9e1c1f0061f
Gary Katsevman <git@gkatsev.com>
2014-12-10 15:34:24 -0500

    Add vjs.fn.isNative isNative will let us know whether a function is native In events.js, don't call addEventListener if it wasn't a native function as it may be an EventEmitter.

--- src/js/lib.js
+++ src/js/lib.js
@@ -838,3 +838,43 @@ vjs.arr.forEach = function(array, callback, thisArg) {
 
   return array;
 };
+
+(function() {
+  // from https://gist.github.com/jdalton/5e34d890105aca44399f
+
+  var toString, fnToString, reHostCtor, reNative;
+
+  toString = Object.prototype.toString;
+
+  // Used to resolve the decompiled source of functions
+  fnToString = Function.prototype.toString;
+
+  // Used to detect host constructors (Safari > 4; really typed array specific)
+  reHostCtor = /^\[object .+?Constructor\]$/;
+
+  // Compile a regexp using a common native method as a template.
+  // We chose `Object#toString` because there's a good chance it is not being mucked with.
+  reNative = RegExp('^' +
+    // Coerce `Object#toString` to a string
+    String(toString)
+    // Escape any special regexp characters
+    .replace(/[.*+?^${}()|[\]\/\\]/g, '\\$&')
+    // Replace mentions of `toString` with `.*?` to keep the template generic.
+    // Replace thing like `for ...` to support environments like Rhino which add extra info
+    // such as method arity.
+    .replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?') + '$'
+  );
+
+  vjs.fn = {};
+  vjs.fn.isNative = function isNative(value) {
+    var type = typeof value;
+    return type == 'function'
+      // Use `Function#toString` to bypass the value's own `toString` method
+      // and avoid being faked out.
+      ? reNative.test(fnToString.call(value))
+      // Fallback to a host object check because some environments will represent
+      // things like typed arrays as DOM methods which may not conform to the
+      // normal native pattern.
+      : (value && type == 'object' && reHostCtor.test(toString.call(value))) || false;
+  }
+})();
