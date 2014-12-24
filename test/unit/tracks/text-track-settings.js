8714151a5ee9e289a0d01281529d83057542ff95
Gary Katsevman <git@gkatsev.com>
2014-12-24 14:49:53 -0500

    No longer have font +/- buttons

--- test/unit/tracks/text-track-settings.js
+++ test/unit/tracks/text-track-settings.js
@@ -16,22 +16,3 @@ test('should close on done click', function() {
   vjs.trigger(player.el().querySelector('.vjs-done-button'), 'click');
   ok(player.textTrackSettings.hasClass('vjs-hidden'), 'settings closed');
 });
-
-test('should increase font on click', function() {
-  var player = PlayerTest.makePlayer(tracks);
-  vjs.trigger(player.el().querySelector('.font-plus'), 'click');
-  equal(player.textTrackSettings.getValues()['fontSize'], '13px', 'font increased');
-});
-
-test('should default on click', function() {
-  var player = PlayerTest.makePlayer(tracks);
-  vjs.trigger(player.el().querySelector('.font-plus'), 'click');
-  vjs.trigger(player.el().querySelector('.vjs-default-button'), 'click');
-  equal(Object.keys(player.textTrackSettings.getValues()), 0, 'settings are set to default');
-});
-
-test('should decrease font on click', function() {
-  var player = PlayerTest.makePlayer(tracks);
-  vjs.trigger(player.el().querySelector('.font-minus'), 'click');
-  equal(player.textTrackSettings.getValues()['fontSize'], '11px', 'font decreased');
-});
