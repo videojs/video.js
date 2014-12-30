2b6635ccba48a0f721170b6e603055601cde054a
jforbes <jforbes@brightcove.com>
2014-12-30 11:54:10 -0500

    Adding some track settings tests

--- test/unit/tracks/text-track-settings.js
+++ test/unit/tracks/text-track-settings.js
@@ -5,6 +5,67 @@ var tracks = [{
   label: 'test'
 }];
 
+test('should update settings', function() {
+  var player = PlayerTest.makePlayer(tracks),
+    newSettings = {
+      'backgroundOpacity': '1',
+      'textOpacity': '1',
+      'windowOpacity': '1',
+      'edgeStyle': 'raised',
+      'fontFamily': 'monospaceSerif',
+      'color': '#FFF',
+      'backgroundColor': '#FFF',
+      'windowColor': '#FFF',
+      'fontPercent': 1.25
+    };
+
+  player.textTrackSettings.setValues(newSettings);
+
+  deepEqual(player.textTrackSettings.getValues(), newSettings, 'values are updated');
+
+  equal(player.el().querySelector('.vjs-fg-color > select').selectedIndex, 1, 'fg-color is set to default value');
+  equal(player.el().querySelector('.vjs-bg-color > select').selectedIndex, 1, 'bg-color is set to default value');
+  equal(player.el().querySelector('.window-color > select').selectedIndex, 1, 'window-color is set to default value');
+  equal(player.el().querySelector('.vjs-text-opacity > select').selectedIndex, 1, 'text-opacity is set to default value');
+  equal(player.el().querySelector('.vjs-bg-opacity > select').selectedIndex, 1, 'bg-opacity is set to default value');
+  equal(player.el().querySelector('.vjs-window-opacity > select').selectedIndex, 1, 'window-opacity is set to default value');
+  equal(player.el().querySelector('.vjs-edge-style select').selectedIndex, 1, 'edge-style is set to default value');
+  equal(player.el().querySelector('.vjs-font-family select').selectedIndex, 1, 'font-family is set to default value');
+  equal(player.el().querySelector('.vjs-font-percent select').selectedIndex, 3, 'font-percent is set to default value');
+});
+
+test('should restore default settings', function() {
+  var player = PlayerTest.makePlayer(tracks);
+
+  player.el().querySelector('.vjs-fg-color > select').selectedIndex = 1;
+  player.el().querySelector('.vjs-bg-color > select').selectedIndex = 1;
+  player.el().querySelector('.window-color > select').selectedIndex = 1;
+  player.el().querySelector('.vjs-text-opacity > select').selectedIndex = 1;
+  player.el().querySelector('.vjs-bg-opacity > select').selectedIndex = 1;
+  player.el().querySelector('.vjs-window-opacity > select').selectedIndex = 1;
+  player.el().querySelector('.vjs-edge-style select').selectedIndex = 1;
+  player.el().querySelector('.vjs-font-family select').selectedIndex = 1;
+  player.el().querySelector('.vjs-font-percent select').selectedIndex = 3;
+
+  vjs.trigger(player.el().querySelector('.vjs-done-button'), 'click');
+  vjs.trigger(player.el().querySelector('.vjs-default-button'), 'click');
+  vjs.trigger(player.el().querySelector('.vjs-done-button'), 'click');
+
+  deepEqual(player.textTrackSettings.getValues(), {
+    'fontPercent': 1.00
+  }, 'values are defaulted');
+
+  equal(player.el().querySelector('.vjs-fg-color > select').selectedIndex, 0, 'fg-color is set to default value');
+  equal(player.el().querySelector('.vjs-bg-color > select').selectedIndex, 0, 'bg-color is set to default value');
+  equal(player.el().querySelector('.window-color > select').selectedIndex, 0, 'window-color is set to default value');
+  equal(player.el().querySelector('.vjs-text-opacity > select').selectedIndex, 0, 'text-opacity is set to default value');
+  equal(player.el().querySelector('.vjs-bg-opacity > select').selectedIndex, 0, 'bg-opacity is set to default value');
+  equal(player.el().querySelector('.vjs-window-opacity > select').selectedIndex, 0, 'window-opacity is set to default value');
+  equal(player.el().querySelector('.vjs-edge-style select').selectedIndex, 0, 'edge-style is set to default value');
+  equal(player.el().querySelector('.vjs-font-family select').selectedIndex, 0, 'font-family is set to default value');
+  equal(player.el().querySelector('.vjs-font-percent select').selectedIndex, 2, 'font-percent is set to default value');
+});
+
 test('should open on click', function() {
   var player = PlayerTest.makePlayer(tracks);
   vjs.trigger(player.el().querySelector('.vjs-texttrack-settings'), 'click');
@@ -13,6 +74,7 @@ test('should open on click', function() {
 
 test('should close on done click', function() {
   var player = PlayerTest.makePlayer(tracks);
+  vjs.trigger(player.el().querySelector('.vjs-texttrack-settings'), 'click');
   vjs.trigger(player.el().querySelector('.vjs-done-button'), 'click');
   ok(player.textTrackSettings.hasClass('vjs-hidden'), 'settings closed');
 });
