2092c48f08b11ad21397c87fdc0b7527b222dbed
Matthew McClure <matt@mmcc.io>
2015-01-07 18:13:20 -0800

    use table layout for IE 8 and 9

--- src/js/player.js
+++ src/js/player.js
@@ -118,6 +118,10 @@ class Player extends Component {
       this.addClass('vjs-audio');
     }
 
+    if (this.flexNotSupported_()) {
+      this.addClass('vjs-no-flex');
+    }
+
     // TODO: Make this smarter. Toggle user state between touching/mousing
     // using events, since devices can have both touch and mouse events.
     // if (Lib.TOUCH_ENABLED) {
@@ -1717,6 +1721,15 @@ Player.prototype.onVolumeChange;
  * @event error
  */
 Player.prototype.onError;
+
+Player.prototype.flexNotSupported_ = function() {
+  var elem = document.createElement('i');
+
+  return !('flexBasis' in elem.style ||
+          'webkitFlexBasis' in elem.style ||
+          'mozFlexBasis' in elem.style ||
+          'msFlexBasis' in elem.style);
+};
 
 Component.registerComponent('Player', Player);
 export default Player;
