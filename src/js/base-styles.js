fa545f0954c6fb0aec5e74370033f4b49fffa1ae
Matthew McClure <matt@mmcc.io>
2015-01-09 22:21:01 -0800

    moved to using extend for icons

--- src/js/base-styles.js
+++ src/js/base-styles.js
@@ -3,6 +3,7 @@
 */
 
 (function prependStylesToHead(styles) {
+  if (window.VIDEOJS_NO_BASE_THEME) return;
   var styleNode = document.createElement('style');
   styleNode.innerHTML = styles;
   document.head.insertBefore(styleNode, document.head.firstChild);
