c4afa17493b4f933e4b47d674208148a82fd1519
Gaurav Saxena <gsaxena@brightcove.com>
2015-04-24 13:17:42 -0400

    address code review comments

--- src/js/media/html5.js
+++ src/js/media/html5.js
@@ -148,8 +148,7 @@ vjs.Html5.prototype.createEl = function(){
     }
 
     vjs.insertFirst(el, player.el());
-  }
-  else {
+  } else {
     // need to make sure we add the video tag back in the UI, when we are changing tech to html5.
     if (el.parentNode !== player.el()) {
       vjs.insertFirst(el, player.el());
