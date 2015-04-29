f21f840afbcb581c65314468a639e2b57591454a
Matthew McClure <matt@mmcc.io>
2015-04-28 17:33:28 -0700

    added scss files to dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,6 +87,12 @@ module.exports = function(grunt) {
           {expand: true, cwd: 'build/temp/', src: ['*'], dest: 'dist/'+version.full+'/', filter: 'isFile'} // includes files in path
         ]
       },
+      scss: {
+        expand: true,
+        cwd: 'src/css',
+        src: ['**/*', '!font/**'],
+        dest: 'build/temp/scss'
+      },
       fonts: { expand: true, cwd: 'src/css/font/', src: ['*'], dest: 'build/temp/font/', filter: 'isFile' },
       swf: { src: './node_modules/videojs-swf/dist/video-js.swf', dest: './build/temp/video-js.swf' },
       novtt: { src: './build/temp/video.js', dest: './build/temp/alt/video.novtt.js' },
@@ -353,6 +359,7 @@ module.exports = function(grunt) {
     'cssmin',
     'copy:fonts',
     'copy:swf',
+    'copy:scss',
     'vjslanguages'
   ]);
 
