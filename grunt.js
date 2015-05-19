f78d091147ca59138dc5724d156b6069950eb2de
jforbes <jforbes@brightcove.com>
2015-05-19 11:31:32 -0400

    Start latest Sauce Connect in test task

--- grunt.js
+++ grunt.js
@@ -188,21 +188,18 @@ module.exports = function(grunt) {
           urls: ['http://127.0.0.1:9999/test/index.html'],
           browsers: [
             /* [platform, browserName, version] - version will default to latest stable version */
-            ['WIN8.1', 'chrome', ''],
-            // TODO: Find out and fix IE and FF on Linux timeouts
-            // ['Linux', 'firefox', ''],
-            // ['WIN8.1', 'internet explorer', ''],
             ['OS X 10.10', 'safari', ''],
+            ['WIN8.1', 'chrome', ''],
+            ['WIN8.1', 'internet explorer', ''],
             ['', 'iPad', '8.2'],
             ['', 'Android', '']
           ],
-          tunneled: false,
+          tunneled: true,
           build: process.env.TRAVIS_BUILD_NUMBER,
           testname: process.env.TRAVIS_BUILD_NUMBER + process.env.TRAVIS_BRANCH,
           throttled: 3,
-          sauceConfig: {
-            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
-          }
+          'max-duration': 500,
+          maxRetries: 3
         }
       }
     },
