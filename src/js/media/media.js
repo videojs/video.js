b7c16419d7cc929dd379df80553643935fd29915
jrivera <jrivera@brightcove.com>
2015-06-04 22:15:10 -0400

    No longer update currentSrc_ synchronously and do not update it at all if src is an empty string

--- src/js/media/media.js
+++ src/js/media/media.js
@@ -495,6 +495,7 @@ vjs.MediaTechController.withSourceHandlers = function(Tech){
    */
   Tech.prototype.setSource = function(source){
     var sh = Tech.selectSourceHandler(source);
+    var tech = this;
 
     if (!sh) {
       // Fall back to a native source hander when unsupported sources are
@@ -510,7 +511,14 @@ vjs.MediaTechController.withSourceHandlers = function(Tech){
     this.disposeSourceHandler();
     this.off('dispose', this.disposeSourceHandler);
 
-    this.currentSource_ = source;
+    // Set currentSource_ asynchronously to simulate the media element's
+    // asynchronous execution of the `resource selection algorithm`
+    setTimeout(function () {
+      if (source && source.src !== '') {
+        tech.currentSource_ = source;
+      }
+    }, 0);
+
     this.sourceHandler_ = sh.handleSource(source, this);
     this.on('dispose', this.disposeSourceHandler);
 
