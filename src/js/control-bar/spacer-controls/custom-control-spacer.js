1cc17eb1c65eaf588475ef900765252c1bc7b45e
Matt Boles <mboles@brightcove.com>
2015-06-17 15:57:05 -0400

    Inserted JSDoc comment for spacer-controls classes

--- src/js/control-bar/spacer-controls/custom-control-spacer.js
+++ src/js/control-bar/spacer-controls/custom-control-spacer.js
@@ -4,14 +4,27 @@ import Component from '../../component.js';
 /**
  * Spacer specifically meant to be used as an insertion point for new plugins, etc.
  *
- * @param {Player|Object} player
- * @param {Obect=} options
+ * @extends Spacer
+ * @class CustomControlSpacer
  */
 class CustomControlSpacer extends Spacer {
+
+  /**
+  * Allow sub components to stack CSS class names
+  *
+  * @return {String} The constructed class name
+  * @method buildCSSClass
+  */
   buildCSSClass() {
     return `vjs-custom-control-spacer ${super.buildCSSClass()}`;
   }
 
+  /**
+  * Create the component's DOM element
+  *
+  * @return {Element}
+  * @method createEl
+  */
   createEl() {
     return super.createEl({
       className: this.buildCSSClass()
--- src/js/control-bar/spacer-controls/spacer.js
+++ src/js/control-bar/spacer-controls/spacer.js
@@ -4,14 +4,28 @@ import Component from '../../component.js';
  * Just an empty spacer element that can be used as an append point for plugins, etc.
  * Also can be used to create space between elements when necessary.
  *
- * @param {Player|Object} player
- * @param {Object=} options
+ * @extends Component
+ * @class Spacer
  */
 class Spacer extends Component {
+
+  /**
+  * Allow sub components to stack CSS class names
+  *
+  * @return {String} The constructed class name
+  * @method buildCSSClass
+  */
   buildCSSClass() {
     return `vjs-spacer ${super.buildCSSClass()}`;
   }
 
+  /**
+  * Create the component's DOM element
+  *
+  * @param {Object} props An object of properties  
+  * @return {Element}
+  * @method createEl
+  */
   createEl(props) {
     return super.createEl('div', {
       className: this.buildCSSClass()
