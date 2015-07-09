a63a28f6a1e01b16eb85b7810d072de7e5763650
Gary Katsevman <git@gkatsev.com>
2015-07-09 16:32:45 -0400

    Export bind on videojs. Fixes #2322

--- src/js/video.js
+++ src/js/video.js
@@ -5,6 +5,7 @@ import globalOptions from './global-options.js';
 import Player from './player';
 import plugin from './plugins.js';
 import mergeOptions from '../../src/js/utils/merge-options.js';
+import * as Fn from './utils/fn.js';
 
 import assign from 'object.assign';
 import log from './utils/log.js';
@@ -242,6 +243,16 @@ videojs.extends = extendsFn;
  * @returns {Object} a new object with the merged values
  */
 videojs.mergeOptions = mergeOptions;
+
+/**
+ * Bind (a.k.a proxy or Context). A simple method for changing the context of a function
+   It also stores a unique id on the function so it can be easily removed from events
+ * @param  {*}   context The object to bind as scope
+ * @param  {Function} fn      The function to be bound to a scope
+ * @param  {Number=}   uid     An optional unique ID for the function to be set
+ * @return {Function}
+ */
+videojs.bind = Fn.bind;
 
 /**
  * Create a Video.js player plugin
