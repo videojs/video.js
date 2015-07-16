a75fabb1a81b4d768e130b1350d156576412d989
Gary Katsevman <git@gkatsev.com>
2015-07-16 15:14:51 -0400

    Use native controls on iphone. Fixes #2365. Also, wrap the trigger in a setTimeout. This is because otherwise, the player will never be able to hear this event since it happens in the constructor of the tech and the player adds listeners after the tech has been created.

--- src/js/tech/html5.js
+++ src/js/tech/html5.js
@@ -73,8 +73,10 @@ class Html5 extends Tech {
     // Our goal should be to get the custom controls on mobile solid everywhere
     // so we can remove this all together. Right now this will block custom
     // controls on touch enabled laptops like the Chrome Pixel
-    if (browser.TOUCH_ENABLED && options.nativeControlsForTouch === true) {
-      this.trigger('usenativecontrols');
+    if ((browser.TOUCH_ENABLED && options.nativeControlsForTouch === true) || browser.IS_IPHONE) {
+      this.setTimeout(function() {
+        this.trigger('usenativecontrols');
+      }, 0);
     }
 
     this.triggerReady();
