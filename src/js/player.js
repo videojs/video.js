960e526e2e9c57219cafa6f0d2d7468287e8dae9
Benoit Tremblay <trembl.ben@gmail.com>
2015-07-18 05:56:41 -0400

    Use poster() instead of the option directly

--- src/js/player.js
+++ src/js/player.js
@@ -503,7 +503,7 @@ class Player extends Component {
       'preload': this.options_.preload,
       'loop': this.options_.loop,
       'muted': this.options_.muted,
-      'poster': this.options_.poster
+      'poster': this.poster()
     }, this.options_[techName.toLowerCase()]);
 
     if (this.tag) {
