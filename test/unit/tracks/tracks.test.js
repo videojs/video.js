cdb0740d1dab232f82b5f2aee4080c4a356d5e97
Gary Katsevman <git@gkatsev.com>
2015-08-11 17:02:38 -0400

    Skip native remove track handler in IE11 or where not supported.

--- test/unit/tracks/tracks.test.js
+++ test/unit/tracks/tracks.test.js
@@ -308,8 +308,8 @@ test('when switching techs, we should not get a new text track', function() {
   ok(htmltracks === flashtracks, 'the tracks are equal');
 });
 
-if (Html5.supportsNativeTextTracks()) {
-  test('listen to remove and add track events in native text tracks', function(assert) {
+if (Html5.supportsNativeTextTracks() && ('removetrack' in Html5.TEST_VID.textTracks)) {
+  test('listen to native remove and add track events in native text tracks', function(assert) {
     let done = assert.async();
 
     let el = document.createElement('video');
@@ -357,6 +357,5 @@ if (Html5.supportsNativeTextTracks()) {
       done();
     };
     emulatedTt.on('addtrack', addtrack);
-
   });
 }
