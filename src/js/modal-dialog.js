7e788c52800a03cf364476225a5aad3a5da75f20
Pat O'Neill <pgoneill@gmail.com>
2015-10-02 16:35:45 -0400

    Add ModalDialog component

---
+++ src/js/modal-dialog.js
@@ -0,0 +1,203 @@
+/**
+ * @file modal-dialog.js
+ */
+import document from 'global/document';
+import tsml from 'tsml';
+
+import * as Dom from './utils/dom';
+import * as Events from './utils/events';
+import * as Fn from './utils/fn';
+
+import Component from './component';
+import CloseButton from './close-button';
+
+const CLASS_NAME_PREFIX = 'vjs-modal-dialog';
+
+// An array of strings which are used for modal dialog sub-element classes,
+// so will cause CSS issues if used as a slug.
+const DISALLOWED_SLUGS = [
+  'content'
+];
+
+const ESC = 27;
+
+/**
+ * Modal dialog for use by plugins to display a dialog over the video, which
+ * blocks interaction with the player until it is closed.
+ *
+ * Modal dialogs include a "Close" button and will close when that button
+ * is activated - or when Esc is pressed anywhere.
+ *
+ * @extends Component
+ * @class ModalDialog
+ */
+class ModalDialog extends Component {
+
+  /**
+   * Constructor for modal dialogs.
+   *
+   * @param  {Player} player
+   * @param  {Object} [options]
+   * @param  {Boolean} [options.disposeOnClose]
+   *         If `true`, the modal can only be opened once. It will be
+   *         disposed as soon as it's closed.
+   *
+   * @param  {String} [options.label]
+   *         A text label for the dialog, primarily for accessibility.
+   *
+   * @param  {Boolean} [options.openImmediately]
+   *         If `true`, the modal will be displayed immediately upon
+   *         instantiation.
+   *
+   * @param  {String} [options.slug]
+   *         This is a string that will be prefixed with "vjs-modal-dialog-"
+   *         to construct a CSS class specific to this modal. For example,
+   *         if this option were "foo," the modal could be identified with
+   *         the class "vjs-modal-dialog-foo".
+   */
+  constructor(player, options) {
+    if (options && DISALLOWED_SLUGS.indexOf(options.slug) > -1) {
+      throw new Error(`${options.slug} is not allowed as a slug`);
+    }
+
+    super(player, options);
+    this.opened_ = false;
+
+    // Create an own property for each instance with a bound keypress
+    // handler so it can be properly added and removed as a listener.
+    this.handleKeyPress = Fn.bind(this, this.handleKeyPress);
+
+    // If a close button was added (which is the default), set the modal to
+    // close when the button is activated.
+    let close = this.getChild('closeButton');
+    if (close) {
+      this.on(close, 'close', this.close);
+    }
+
+    // Make sure the contentEl is defined AFTER any children are initialized
+    // because we only want the contents of the modal in the contentEl (not
+    // the UI elements like the close button).
+    this.contentEl_ = Dom.createEl('div', {
+      className: `${CLASS_NAME_PREFIX}-content`
+    });
+    this.el_.appendChild(this.contentEl_);
+
+    if (this.options_.openImmediately) {
+      this.open();
+    }
+  }
+
+  /**
+   * Create the modal dialog's DOM element
+   *
+   * @method createEl
+   */
+  createEl() {
+    return super.createEl('div', {
+      className: this.buildCSSClass(),
+      tabIndex: -1
+    }, {
+      'aria-role': 'dialog',
+      'aria-label': this.options_.label || ''
+    });
+  }
+
+  /**
+   * Build the modal dialog's CSS class.
+   *
+   * @method buildCSSClass
+   */
+  buildCSSClass() {
+    return `${CLASS_NAME_PREFIX} vjs-hidden ${CLASS_NAME_PREFIX}-${this.options_.slug} ${super.buildCSSClass()}`;
+  }
+
+  /**
+   * Handles key presses on the document, looking for ESC, which closes
+   * the modal dialog.
+   *
+   * @method handleKeyPress
+   * @param  {Event} e
+   */
+  handleKeyPress(e) {
+    if (e.which === ESC) {
+      this.close();
+    }
+  }
+
+  /**
+   * Opens the modal dialog.
+   *
+   * @method open
+   * @return {ModalDialog}
+   */
+  open() {
+    if (this.opened_) {
+      return;
+    }
+
+    this.trigger('beforemodalopen');
+    this.opened_ = true;
+
+    // If the player was playing, pause it and take note of its previously
+    // playing state.
+    this.wasPlaying_ = !this.player().paused();
+    if (this.wasPlaying_) {
+      this.player().pause();
+    }
+
+    Events.on(document, 'keydown', this.handleKeyPress);
+    this.show();
+    this.trigger('modalopen');
+  }
+
+  /**
+   * Whether or not the modal is open currently.
+   *
+   * @method opened
+   * @return {Boolean}
+   */
+  opened() {
+    return !!this.opened_;
+  }
+
+  /**
+   * Closes the modal dialog.
+   *
+   * @method close
+   * @return {ModalDialog}
+   */
+  close() {
+    if (!this.opened_) {
+      return;
+    }
+
+    this.trigger('beforemodalclose');
+    this.opened_ = false;
+
+    if (this.wasPlaying_) {
+      this.player().play();
+    }
+
+    Events.off(document, 'keydown', this.handleKeyPress);
+    this.hide();
+    this.trigger('modalclose');
+
+    if (this.options_.disposeOnClose) {
+      this.dispose();
+    }
+  }
+}
+
+/*
+ * Modal dialog default options.
+ *
+ * @type {Object}
+ * @private
+ */
+ModalDialog.prototype.options_ = {
+  children: ['closeButton'],
+  slug: 'none'
+};
+
+Component.registerComponent('ModalDialog', ModalDialog);
+export default ModalDialog;
---
+++ test/unit/modal-dialog.test.js
@@ -0,0 +1,209 @@
+import CloseButton from '../../src/js/close-button';
+import ModalDialog from '../../src/js/modal-dialog';
+import * as Fn from '../../src/js/utils/fn';
+import TestHelpers from './test-helpers';
+
+q.module('ModalDialog', {
+
+  beforeEach: function() {
+    this.player = TestHelpers.makePlayer();
+    this.modal = new ModalDialog(this.player);
+    this.el = this.modal.el();
+  },
+
+  afterEach: function() {
+    this.player.dispose();
+    this.modal.dispose();
+  }
+});
+
+q.test('should create the expected element', function(assert) {
+  var classes = [
+    'modal-dialog',
+    'hidden',
+    'modal-dialog-none'
+  ];
+
+  assert.expect(4 + classes.length);
+  assert.strictEqual(this.el.tagName.toLowerCase(), 'div', 'el is a <div>');
+  assert.strictEqual(this.el.tabIndex, -1, 'el has -1 tabindex');
+  assert.strictEqual(this.el.getAttribute('aria-role'), 'dialog', 'el has aria-role="dialog"');
+  assert.strictEqual(this.el.getAttribute('aria-label'), '', 'el has aria-role="" by default');
+
+  classes.forEach(function(s) {
+    var c = 'vjs-' + s;
+    assert.ok(this.modal.hasClass(c), [
+      'has the "',
+      c,
+      '" class in "',
+      this.el.className,
+      '"'
+    ].join(''));
+  }, this);
+});
+
+q.test('should create the expected contentEl', function(assert) {
+  var contentEl = this.modal.contentEl();
+
+  assert.expect(3);
+  assert.strictEqual(contentEl.parentNode, this.el, 'contentEl is a child of el');
+  assert.strictEqual(contentEl.tagName.toLowerCase(), 'div', 'contentEl is a <div>');
+  assert.strictEqual(contentEl.className.trim(), 'vjs-modal-dialog-content', 'has "vjs-modal-dialog-content" class');
+});
+
+q.test('should create a close button by default', function(assert) {
+  var btn = this.modal.getChild('closeButton');
+
+  // We only check the aspects of the button that relate to the modal. Other
+  // aspects of the button (classes, etc) are tested in their appropriate test
+  // module.
+  assert.expect(2);
+  assert.ok(btn instanceof CloseButton, 'close button is a CloseButton');
+  assert.strictEqual(btn.el().parentNode, this.el, 'close button is a child of el');
+});
+
+q.test('open() triggers events', function(assert) {
+  var modal = this.modal;
+  var beforeModalOpenSpy = sinon.spy(function() {
+    assert.notOk(modal.opened(), 'modal is not opened before opening event');
+  });
+
+  var modalOpenSpy = sinon.spy(function() {
+    assert.ok(modal.opened(), 'modal is opened on opening event');
+  });
+
+  modal.on('beforemodalopen', beforeModalOpenSpy);
+  modal.on('modalopen', modalOpenSpy);
+
+  assert.expect(4);
+  modal.open();
+  assert.strictEqual(beforeModalOpenSpy.callCount, 1, 'beforemodalopen spy was called');
+  assert.strictEqual(modalOpenSpy.callCount, 1, 'modalopen spy was called');
+});
+
+q.test('open() removes "vjs-hidden" class', function(assert) {
+  assert.expect(2);
+  assert.ok(this.modal.hasClass('vjs-hidden'), 'modal starts hidden');
+  this.modal.open();
+  assert.notOk(this.modal.hasClass('vjs-hidden'), 'modal is not hidden after opening');
+});
+
+q.test('open() cannot be called on an opened modal', function(assert) {
+  var spy = sinon.spy();
+
+  this.modal.on('modalopen', spy);
+  this.modal.open();
+  this.modal.open();
+
+  assert.expect(1);
+  assert.strictEqual(spy.callCount, 1, 'modal was only opened once');
+});
+
+q.test('close() triggers events', function(assert) {
+  var modal = this.modal;
+  var beforeModalCloseSpy = sinon.spy(function() {
+    assert.ok(modal.opened(), 'modal is not closed before closing event');
+  });
+
+  var modalCloseSpy = sinon.spy(function() {
+    assert.notOk(modal.opened(), 'modal is closed on closing event');
+  });
+
+  modal.on('beforemodalclose', beforeModalCloseSpy);
+  modal.on('modalclose', modalCloseSpy);
+
+  assert.expect(4);
+  modal.open();
+  modal.close();
+  assert.strictEqual(beforeModalCloseSpy.callCount, 1, 'beforemodalclose spy was called');
+  assert.strictEqual(modalCloseSpy.callCount, 1, 'modalclose spy was called');
+});
+
+q.test('close() adds the "vjs-hidden" class', function(assert) {
+  assert.expect(1);
+  this.modal.open();
+  this.modal.close();
+  assert.ok(this.modal.hasClass('vjs-hidden'), 'modal is hidden upon close');
+});
+
+q.test('pressing ESC triggers close(), but only when the modal is opened', function(assert) {
+  var spy = sinon.spy();
+
+  this.modal.on('modalclose', spy);
+  this.modal.handleKeyPress({which: 27});
+  assert.expect(2);
+  assert.strictEqual(spy.callCount, 0, 'ESC did not close the closed modal');
+
+  this.modal.open();
+  this.modal.handleKeyPress({which: 27});
+  assert.strictEqual(spy.callCount, 1, 'ESC closed the now-opened modal');
+});
+
+q.test('close() cannot be called on an closed modal', function(assert) {
+  var spy = sinon.spy();
+
+  this.modal.on('modalclose', spy);
+  this.modal.open();
+  this.modal.close();
+  this.modal.close();
+
+  assert.expect(1);
+  assert.strictEqual(spy.callCount, 1, 'modal was only closed once');
+});
+
+q.test('open() pauses playback, close() resumes', function(assert) {
+
+  // Quick and dirty; make it looks like the player is playing.
+  this.player.paused = function() {
+    return false;
+  };
+
+  sinon.spy(this.player, 'play');
+  sinon.spy(this.player, 'pause');
+  this.modal.open();
+
+  assert.expect(2);
+  assert.strictEqual(this.player.pause.callCount, 1, 'player is paused when the modal opens');
+
+  this.modal.close();
+  assert.strictEqual(this.player.play.callCount, 1, 'player is resumed when the modal closes');
+});
+
+q.test('"disposeOnClose" option', function(assert) {
+  var modal = new ModalDialog(this.player, {disposeOnClose: true});
+
+  modal.open();
+  sinon.spy(modal, 'dispose');
+  modal.close();
+
+  assert.expect(1);
+  assert.strictEqual(modal.dispose.callCount, 1, 'dispose was called');
+});
+
+q.test('"label" option', function(assert) {
+  var label = 'foo';
+  var modal = new ModalDialog(this.player, {label: label});
+
+  assert.expect(1);
+  assert.strictEqual(modal.el().getAttribute('aria-label'), label, 'uses the label as the aria-label');
+});
+
+q.test('"openImmediately" option', function(assert) {
+  var modal = new ModalDialog(this.player, {openImmediately: true});
+
+  assert.expect(1);
+  assert.ok(modal.opened(), 'the modal is opened immediately');
+});
+
+q.test('"slug" option', function(assert) {
+  var player = this.player;
+  var slug = 'foo';
+  var modal = new ModalDialog(player, {slug: slug});
+
+  assert.expect(2);
+  assert.ok(modal.hasClass('vjs-modal-dialog-' + slug), 'adds the slug-based class');
+
+  assert.throws(function() {
+    new ModalDialog(player, {slug: 'content'});
+  }, 'throws errors on disallowed slugs');
+});
