ca5d28ab0c290eeac919fa1fb21ea7aeb7d7598f
Gary Katsevman <git@gkatsev.com>
2015-10-19 19:48:59 -0400

    Revert "Add another assertion"
    This reverts commit f4e56754935da5e17d6e4268593dea3cedfe211a.

--- test/unit/utils/url.test.js
+++ test/unit/utils/url.test.js
@@ -79,8 +79,6 @@ test('isCrossOrigin can identify cross origin urls', function() {
     'global/window': win
   });
 
-  ok(Url.isCrossOrigin, 'we export a isCrossOrigin function');
-
   win.location.protocol = window.location.protocol;
   win.location.host = window.location.host;
   ok(!Url.isCrossOrigin(`http://${win.location.host}/example.vtt`), 'http://google.com from http://google.com is not cross origin');
