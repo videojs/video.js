448d2ffbd9db1d549a78b16eea1b91fe49a64969
Carey Hinoki <carey.r.hinoki.-nd@disney.com>
2015-11-10 18:31:40 -0800

    Implement `HTMLTrackElement` shim.
    - Add communication between emulated `HTMLTrackElement` and corresponding emulated `TextTrack`
    - Followed `TextTrack` shim

---
+++ src/js/tracks/html-track-element.js
@@ -0,0 +1,81 @@
+import * as browser from '../utils/browser.js';
+import EventTarget from '../event-target';
+
+/**
+ * https://html.spec.whatwg.org/multipage/embedded-content.html#htmltrackelement
+ *
+ * interface HTMLTrackElement : HTMLElement {
+ *   attribute DOMString kind;
+ *   attribute DOMString src;
+ *   attribute DOMString srclang;
+ *   attribute DOMString label;
+ *   attribute boolean default;
+ *
+ *   const unsigned short NONE = 0;
+ *   const unsigned short LOADING = 1;
+ *   const unsigned short LOADED = 2;
+ *   const unsigned short ERROR = 3;
+ *   readonly attribute unsigned short readyState;
+ *
+ *   readonly attribute TextTrack track;
+ * };
+ */
+
+class HTMLTrackElement extends EventTarget {
+  constructor(track) {
+    super();
+
+    var readyState,
+        textTrack,
+        trackElement = this;
+
+    if (browser.IS_IE8) {
+      // TODO: do stuff
+    }
+
+    this.kind = track.kind;
+    this.src = track.src;
+    this.srclang = track.srclang;
+    this.label = track.label;
+    this.default = track.default;
+
+    const NONE = 0;
+    const LOADING = 1;
+    const LOADED = 2;
+    const ERROR = 3;
+
+    Object.defineProperty(this, 'readyState', {
+      get() {
+        return readyState;
+      }
+    });
+
+    Object.defineProperty(this, 'track', {
+      get() {
+        return textTrack;
+      }
+    });
+
+    readyState = NONE;
+    textTrack = track;
+
+    this.allowedEvents_ = {
+      load: 'load'
+    };
+
+    textTrack.addEventListener('loadeddata', function() {
+      readyState = LOADED;
+
+      trackElement.trigger({
+        type: 'load',
+        target: trackElement
+      });
+    });
+
+    if (browser.IS_IE8) {
+      return this;
+    }
+  }
+}
+
+export default HTMLTrackElement;
