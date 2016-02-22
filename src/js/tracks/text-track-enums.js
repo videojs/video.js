1e5937bf72d0733c3b1bb3f7dd070ff2ba1b7722
brandonocasey <brandonocasey@gmail.com>
2016-02-22 13:52:15 -0500

    converted remaining text-track modules to full es6 added jsdoc comments to functions that did not previously have them linted changed files with vjsstandard

--- src/js/tracks/text-track-enums.js
+++ src/js/tracks/text-track-enums.js
@@ -1,27 +1,39 @@
 /**
  * @file text-track-enums.js
- *
+ */
+
+/**
  * https://html.spec.whatwg.org/multipage/embedded-content.html#texttrackmode
  *
  * enum TextTrackMode { "disabled",  "hidden",  "showing" };
  */
-var TextTrackMode = {
-  'disabled': 'disabled',
-  'hidden': 'hidden',
-  'showing': 'showing'
+const TextTrackMode = {
+  disabled: 'disabled',
+  hidden: 'hidden',
+  showing: 'showing'
 };
 
-/*
+/**
  * https://html.spec.whatwg.org/multipage/embedded-content.html#texttrackkind
  *
- * enum TextTrackKind { "subtitles",  "captions",  "descriptions",  "chapters",  "metadata" };
+ * enum TextTrackKind {
+ *   "subtitles",
+ *   "captions",
+ *   "descriptions",
+ *   "chapters",
+ *   "metadata"
+ * };
  */
-var TextTrackKind = {
-  'subtitles': 'subtitles',
-  'captions': 'captions',
-  'descriptions': 'descriptions',
-  'chapters': 'chapters',
-  'metadata': 'metadata'
+const TextTrackKind = {
+  subtitles: 'subtitles',
+  captions: 'captions',
+  descriptions: 'descriptions',
+  chapters: 'chapters',
+  metadata: 'metadata'
 };
 
+/* jshint ignore:start */
+// we ignore jshint here because it does not see
+// TextTrackMode or TextTrackKind as defined here somehow...
 export { TextTrackMode, TextTrackKind };
+/* jshint ignore:end */
--- src/js/tracks/text-track.js
+++ src/js/tracks/text-track.js
@@ -13,229 +13,13 @@ import window from 'global/window';
 import { isCrossOrigin } from '../utils/url.js';
 import XHR from 'xhr';
 
-/*
- * https://html.spec.whatwg.org/multipage/embedded-content.html#texttrack
- *
- * interface TextTrack : EventTarget {
- *   readonly attribute TextTrackKind kind;
- *   readonly attribute DOMString label;
- *   readonly attribute DOMString language;
- *
- *   readonly attribute DOMString id;
- *   readonly attribute DOMString inBandMetadataTrackDispatchType;
- *
- *   attribute TextTrackMode mode;
- *
- *   readonly attribute TextTrackCueList? cues;
- *   readonly attribute TextTrackCueList? activeCues;
- *
- *   void addCue(TextTrackCue cue);
- *   void removeCue(TextTrackCue cue);
- *
- *   attribute EventHandler oncuechange;
- * };
+/**
+ * Downloading stuff happens below this point
  */
-function TextTrack (options={}) {
-  if (!options.tech) {
-    throw new Error('A tech was not provided.');
-  }
-
-  let tt = this;
-  if (browser.IS_IE8) {
-    tt = document.createElement('custom');
-
-    for (let prop in TextTrack.prototype) {
-      if (prop !== 'constructor') {
-        tt[prop] = TextTrack.prototype[prop];
-      }
-    }
-  }
-
-  tt.tech_ = options.tech;
-
-  let mode = TextTrackEnum.TextTrackMode[options['mode']] || 'disabled';
-  let kind = TextTrackEnum.TextTrackKind[options['kind']] || 'subtitles';
-  let label = options['label'] || '';
-  let language = options['language'] || options['srclang'] || '';
-  let id = options['id'] || 'vjs_text_track_' + Guid.newGUID();
-
-  if (kind === 'metadata' || kind === 'chapters') {
-    mode = 'hidden';
-  }
-
-  tt.cues_ = [];
-  tt.activeCues_ = [];
-
-  let cues = new TextTrackCueList(tt.cues_);
-  let activeCues = new TextTrackCueList(tt.activeCues_);
-
-  let changed = false;
-  let timeupdateHandler = Fn.bind(tt, function() {
-    this['activeCues'];
-    if (changed) {
-      this['trigger']('cuechange');
-      changed = false;
-    }
-  });
-  if (mode !== 'disabled') {
-    tt.tech_.on('timeupdate', timeupdateHandler);
-  }
-
-  Object.defineProperty(tt, 'kind', {
-    get: function() {
-      return kind;
-    },
-    set: Function.prototype
-  });
-
-  Object.defineProperty(tt, 'label', {
-    get: function() {
-      return label;
-    },
-    set: Function.prototype
-  });
-
-  Object.defineProperty(tt, 'language', {
-    get: function() {
-      return language;
-    },
-    set: Function.prototype
-  });
-
-  Object.defineProperty(tt, 'id', {
-    get: function() {
-      return id;
-    },
-    set: Function.prototype
-  });
-
-  Object.defineProperty(tt, 'mode', {
-    get: function() {
-      return mode;
-    },
-    set: function(newMode) {
-      if (!TextTrackEnum.TextTrackMode[newMode]) {
-        return;
-      }
-      mode = newMode;
-      if (mode === 'showing') {
-        this.tech_.on('timeupdate', timeupdateHandler);
-      }
-      this.trigger('modechange');
-    }
-  });
-
-  Object.defineProperty(tt, 'cues', {
-    get: function() {
-      if (!this.loaded_) {
-        return null;
-      }
-
-      return cues;
-    },
-    set: Function.prototype
-  });
-
-  Object.defineProperty(tt, 'activeCues', {
-    get: function() {
-      if (!this.loaded_) {
-        return null;
-      }
-
-      if (this['cues'].length === 0) {
-        return activeCues; // nothing to do
-      }
-
-      let ct = this.tech_.currentTime();
-      let active = [];
-
-      for (let i = 0, l = this['cues'].length; i < l; i++) {
-        let cue = this['cues'][i];
-        if (cue['startTime'] <= ct && cue['endTime'] >= ct) {
-          active.push(cue);
-        } else if (cue['startTime'] === cue['endTime'] && cue['startTime'] <= ct && cue['startTime'] + 0.5 >= ct) {
-          active.push(cue);
-        }
-      }
-
-      changed = false;
-
-      if (active.length !== this.activeCues_.length) {
-        changed = true;
-      } else {
-        for (let i = 0; i < active.length; i++) {
-          if (indexOf.call(this.activeCues_, active[i]) === -1) {
-            changed = true;
-          }
-        }
-      }
-
-      this.activeCues_ = active;
-      activeCues.setCues_(this.activeCues_);
-
-      return activeCues;
-    },
-    set: Function.prototype
-  });
-
-  if (options.src) {
-    tt.src = options.src;
-    loadTrack(options.src, tt);
-  } else {
-    tt.loaded_ = true;
-  }
-
-  if (browser.IS_IE8) {
-    return tt;
-  }
-}
-
-TextTrack.prototype = Object.create(EventTarget.prototype);
-TextTrack.prototype.constructor = TextTrack;
-
-/*
- * cuechange - One or more cues in the track have become active or stopped being active.
- */
-TextTrack.prototype.allowedEvents_ = {
-  'cuechange': 'cuechange'
-};
-
-TextTrack.prototype.addCue = function(cue) {
-  let tracks = this.tech_.textTracks();
-
-  if (tracks) {
-    for (let i = 0; i < tracks.length; i++) {
-      if (tracks[i] !== this) {
-        tracks[i].removeCue(cue);
-      }
-    }
-  }
-
-  this.cues_.push(cue);
-  this['cues'].setCues_(this.cues_);
-};
-
-TextTrack.prototype.removeCue = function(removeCue) {
-  let removed = false;
-
-  for (let i = 0, l = this.cues_.length; i < l; i++) {
-    let cue = this.cues_[i];
-    if (cue === removeCue) {
-      this.cues_.splice(i, 1);
-      removed = true;
-    }
-  }
-
-  if (removed) {
-    this.cues.setCues_(this.cues_);
-  }
-};
-
-/*
-* Downloading stuff happens below this point
-*/
-var parseCues = function(srcContent, track) {
-  let parser = new window.WebVTT.Parser(window, window.vttjs, window.WebVTT.StringDecoder());
+const parseCues = function(srcContent, track) {
+  let parser = new window.WebVTT.Parser(window,
+                                        window.vttjs,
+                                        window.WebVTT.StringDecoder());
 
   parser.oncue = function(cue) {
     track.addCue(cue);
@@ -256,17 +40,17 @@ var parseCues = function(srcContent, track) {
   parser.flush();
 };
 
-var loadTrack = function(src, track) {
+const loadTrack = function(src, track) {
   let opts = {
     uri: src
   };
-
   let crossOrigin = isCrossOrigin(src);
+
   if (crossOrigin) {
     opts.cors = crossOrigin;
   }
 
-  XHR(opts, Fn.bind(this, function(err, response, responseBody){
+  XHR(opts, Fn.bind(this, function(err, response, responseBody) {
     if (err) {
       return log.error(err, response);
     }
@@ -284,7 +68,7 @@ var loadTrack = function(src, track) {
   }));
 };
 
-var indexOf = function(searchElement, fromIndex) {
+const indexOf = function(searchElement, fromIndex) {
   if (this == null) {
     throw new TypeError('"this" is null or not defined');
   }
@@ -316,6 +100,247 @@ var indexOf = function(searchElement, fromIndex) {
     k++;
   }
   return -1;
+};
+
+/**
+ * A single text track as defined in:
+ * https://html.spec.whatwg.org/multipage/embedded-content.html#texttrack
+ *
+ * interface TextTrack : EventTarget {
+ *   readonly attribute TextTrackKind kind;
+ *   readonly attribute DOMString label;
+ *   readonly attribute DOMString language;
+ *
+ *   readonly attribute DOMString id;
+ *   readonly attribute DOMString inBandMetadataTrackDispatchType;
+ *
+ *   attribute TextTrackMode mode;
+ *
+ *   readonly attribute TextTrackCueList? cues;
+ *   readonly attribute TextTrackCueList? activeCues;
+ *
+ *   void addCue(TextTrackCue cue);
+ *   void removeCue(TextTrackCue cue);
+ *
+ *   attribute EventHandler oncuechange;
+ * };
+ *
+ * @param {Object=} options Object of option names and values
+ * @extends EventTarget
+ * @class TextTrack
+ */
+class TextTrack extends EventTarget {
+  constructor(options = {}) {
+    super();
+    if (!options.tech) {
+      throw new Error('A tech was not provided.');
+    }
+
+    let tt = this;
+
+    if (browser.IS_IE8) {
+      tt = document.createElement('custom');
+
+      for (let prop in TextTrack.prototype) {
+        if (prop !== 'constructor') {
+          tt[prop] = TextTrack.prototype[prop];
+        }
+      }
+    }
+
+    tt.tech_ = options.tech;
+
+    let mode = TextTrackEnum.TextTrackMode[options.mode] || 'disabled';
+    let kind = TextTrackEnum.TextTrackKind[options.kind] || 'subtitles';
+    let label = options.label || '';
+    let language = options.language || options.srclang || '';
+    let id = options.id || 'vjs_text_track_' + Guid.newGUID();
+
+    if (kind === 'metadata' || kind === 'chapters') {
+      mode = 'hidden';
+    }
+
+    tt.cues_ = [];
+    tt.activeCues_ = [];
+
+    let cues = new TextTrackCueList(tt.cues_);
+    let activeCues = new TextTrackCueList(tt.activeCues_);
+    let changed = false;
+    let timeupdateHandler = Fn.bind(tt, function() {
+      this.activeCues;
+      if (changed) {
+        this.trigger('cuechange');
+        changed = false;
+      }
+    });
+
+    if (mode !== 'disabled') {
+      tt.tech_.on('timeupdate', timeupdateHandler);
+    }
+
+    Object.defineProperty(tt, 'kind', {
+      get() {
+        return kind;
+      },
+      set: Function.prototype
+    });
+
+    Object.defineProperty(tt, 'label', {
+      get() {
+        return label;
+      },
+      set: Function.prototype
+    });
+
+    Object.defineProperty(tt, 'language', {
+      get() {
+        return language;
+      },
+      set: Function.prototype
+    });
+
+    Object.defineProperty(tt, 'id', {
+      get() {
+        return id;
+      },
+      set: Function.prototype
+    });
+
+    Object.defineProperty(tt, 'mode', {
+      get() {
+        return mode;
+      },
+      set(newMode) {
+        if (!TextTrackEnum.TextTrackMode[newMode]) {
+          return;
+        }
+        mode = newMode;
+        if (mode === 'showing') {
+          this.tech_.on('timeupdate', timeupdateHandler);
+        }
+        this.trigger('modechange');
+      }
+    });
+
+    Object.defineProperty(tt, 'cues', {
+      get() {
+        if (!this.loaded_) {
+          return null;
+        }
+
+        return cues;
+      },
+      set: Function.prototype
+    });
+
+    Object.defineProperty(tt, 'activeCues', {
+      get() {
+        if (!this.loaded_) {
+          return null;
+        }
+
+        // nothing to do
+        if (this.cues.length === 0) {
+          return activeCues;
+        }
+
+        let ct = this.tech_.currentTime();
+        let active = [];
+
+        for (let i = 0, l = this.cues.length; i < l; i++) {
+          let cue = this.cues[i];
+
+          if (cue.startTime <= ct && cue.endTime >= ct) {
+            active.push(cue);
+          } else if (cue.startTime === cue.endTime &&
+                     cue.startTime <= ct &&
+                     cue.startTime + 0.5 >= ct) {
+            active.push(cue);
+          }
+        }
+
+        changed = false;
+
+        if (active.length !== this.activeCues_.length) {
+          changed = true;
+        } else {
+          for (let i = 0; i < active.length; i++) {
+            if (indexOf.call(this.activeCues_, active[i]) === -1) {
+              changed = true;
+            }
+          }
+        }
+
+        this.activeCues_ = active;
+        activeCues.setCues_(this.activeCues_);
+
+        return activeCues;
+      },
+      set: Function.prototype
+    });
+
+    if (options.src) {
+      tt.src = options.src;
+      loadTrack(options.src, tt);
+    } else {
+      tt.loaded_ = true;
+    }
+
+    if (browser.IS_IE8) {
+      return tt;
+    }
+  }
+
+  /**
+   * add a cue to the internal list of cues
+   *
+   * @param {Object} cue the cue to add to our internal list
+   * @method addCue
+   */
+  addCue(cue) {
+    let tracks = this.tech_.textTracks();
+
+    if (tracks) {
+      for (let i = 0; i < tracks.length; i++) {
+        if (tracks[i] !== this) {
+          tracks[i].removeCue(cue);
+        }
+      }
+    }
+
+    this.cues_.push(cue);
+    this.cues.setCues_(this.cues_);
+  }
+
+  /**
+   * remvoe a cue from our internal list
+   *
+   * @param {Object} removeCue the cue to remove from our internal list
+   * @method removeCue
+   */
+  removeCue(removeCue) {
+    let removed = false;
+
+    for (let i = 0, l = this.cues_.length; i < l; i++) {
+      let cue = this.cues_[i];
+
+      if (cue === removeCue) {
+        this.cues_.splice(i, 1);
+        removed = true;
+      }
+    }
+
+    if (removed) {
+      this.cues.setCues_(this.cues_);
+    }
+  }
+}
+
+/**
+ * cuechange - One or more cues in the track have become active or stopped being active.
+ */
+TextTrack.prototype.allowedEvents_ = {
+  cuechange: 'cuechange'
 };
 
 export default TextTrack;
