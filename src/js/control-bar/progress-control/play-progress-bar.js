923c093f26c67a101358e9aeb91e789f77b2e4c1
Gary Katsevman <git@gkatsev.com>
2016-02-29 16:03:26 -0500

    try using a tooltip element rather than a pseudo element

--- src/js/control-bar/progress-control/play-progress-bar.js
+++ src/js/control-bar/progress-control/play-progress-bar.js
@@ -3,6 +3,7 @@
  */
 import Component from '../../component.js';
 import * as Fn from '../../utils/fn.js';
+import * as Dom from '../../utils/dom.js';
 import formatTime from '../../utils/format-time.js';
 
 /**
@@ -20,6 +21,23 @@ class PlayProgressBar extends Component {
     this.updateDataAttr();
     this.on(player, 'timeupdate', this.updateDataAttr);
     player.ready(Fn.bind(this, this.updateDataAttr));
+
+    if (options.playerOptions &&
+        options.playerOptions.controlBar &&
+        options.playerOptions.controlBar.progressControl &&
+        options.playerOptions.controlBar.progressControl.keepWithin) {
+      this.keepWithin = options.playerOptions.controlBar.progressControl.keepWithin;
+    }
+
+    if (this.keepWithin) {
+      this.timeTooltip = Dom.createEl('div', {
+        className: 'vjs-play-progress-time vjs-time-tooltip'
+      });
+      this.el().appendChild(this.timeTooltip);
+
+      this.on(player, ['seeked'], this.resetMargin);
+      this.addClass('vjs-keep-within');
+    }
   }
 
   /**
@@ -35,9 +53,30 @@ class PlayProgressBar extends Component {
     });
   }
 
+  resetMargin() {
+    this.timeTooltip.style.marginRight = '-1.5em';
+  }
+
   updateDataAttr() {
     let time = (this.player_.scrubbing()) ? this.player_.getCache().currentTime : this.player_.currentTime();
     this.el_.setAttribute('data-current-time', formatTime(time, this.player_.duration()));
+
+    if (this.keepWithin) {
+      let progressControl = player.controlBar.progressControl;
+      let playerRect = progressControl.el().getBoundingClientRect();
+      let tooltipRect = this.timeTooltip.getBoundingClientRect();
+      let tooltipStyle = getComputedStyle(this.timeTooltip);
+
+      if (tooltipRect.right === 0 && tooltipRect.left === 0) {
+        return;
+      }
+
+      if (playerRect.right < tooltipRect.right) {
+        this.timeTooltip.style.marginRight = (parseInt(tooltipStyle.marginRight, 10) - (playerRect.right - tooltipRect.right)) + 'px';
+      }
+
+      this.timeTooltip.innerHTML = formatTime(time, this.player_.duration());
+    }
   }
 
 }
