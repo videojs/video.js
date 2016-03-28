fa04c467f4e00d8e3d5f16d39d5fc6d4e8744106
Maythee Anegboonlap <maythee@viki.com>
2016-03-28 16:35:52 +0800

    Add specs for pressButton checking

--- test/unit/menu.test.js
+++ test/unit/menu.test.js
@@ -4,6 +4,22 @@ import * as Events from '../../src/js/utils/events.js';
 
 q.module('MenuButton');
 
+q.test('should not throw an error when there is no children', function() {
+  expect(0);
+  let player = TestHelpers.makePlayer();
+
+  let menuButton = new MenuButton(player);
+  let el = menuButton.el();
+
+  try {
+    Events.trigger(el, 'click');
+  } catch (error) {
+    ok(!error, 'click should not throw anything');
+  }
+
+  player.dispose();
+});
+
 q.test('should place title list item into ul', function() {
   let player = TestHelpers.makePlayer();
 
