b18864a8b89e4c3dbaf4be916e9cde7c653530d0
Maythee Anegboonlap <maythee@viki.com>
2016-03-29 10:15:35 +0800

    Add missing Events after rebase

--- test/unit/menu.test.js
+++ test/unit/menu.test.js
@@ -1,23 +1,8 @@
 import MenuButton from '../../src/js/menu/menu-button.js';
 import TestHelpers from './test-helpers.js';
+import * as Events from '../../src/js/utils/events.js';
 
 q.module('MenuButton');
-
-q.test('should not throw an error when there is no children', function() {
-  expect(0);
-  let player = TestHelpers.makePlayer();
-
-  let menuButton = new MenuButton(player);
-  let el = menuButton.el();
-
-  try {
-    Events.trigger(el, 'click');
-  } catch (error) {
-    ok(!error, 'click should not throw anything');
-  }
-
-  player.dispose();
-});
 
 q.test('should not throw an error when there is no children', function() {
   expect(0);
