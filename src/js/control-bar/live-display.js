04bb8f8e5dc7221c40b130ae0b573550be947a50
mister-ben <git@misterben.me>
2016-04-12 10:53:14 +0300

    Revert CSS and check live staus in LiveDisplay

--- src/js/control-bar/live-display.js
+++ src/js/control-bar/live-display.js
@@ -3,6 +3,7 @@
  */
 import Component from '../component';
 import * as Dom from '../utils/dom.js';
+import * as browser from '../utils/browser.js';
 
 /**
  * Displays the live indicator
@@ -12,6 +13,13 @@ import * as Dom from '../utils/dom.js';
  * @class LiveDisplay
  */
 class LiveDisplay extends Component {
+
+  constructor(player, options) {
+    super(player, options);
+
+    this.updateShowing();
+    this.on(this.player(), 'durationchange', this.updateShowing);
+  }
 
   /**
    * Create the component's DOM element
@@ -33,6 +41,31 @@ class LiveDisplay extends Component {
 
     el.appendChild(this.contentEl_);
     return el;
+  }
+
+  updateShowing() {
+    if (this.player().duration() === Infinity) {
+      // On Android Chrome, duration of VOD HLS remains Infinity until
+      // after playback has begun (and after hasStarted()).
+      if (browser.IS_ANDROID && browser.IS_CHROME) {
+        let checkDuration = function () {
+          if (this.player().currentTime() > 0) {
+            if (this.player().duration() === Infinity) {
+              this.show();
+            } else {
+              this.hide();
+            }
+            this.off(this.player(), 'timeupdate', checkDuration);
+          }
+        };
+
+        this.on(this.player(), 'timeupdate', checkDuration);
+      } else {
+        this.show();
+      }
+    } else {
+      this.hide();
+    }
   }
 
 }
