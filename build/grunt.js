f6388c99d410a261ae0cd8b6c1317eb642843800
Erik Yuzwa <erik@erikyuzwa.com>
2016-06-20 17:44:03 -0600

    modified grunt for ie8 injection into main css

--- build/grunt.js
+++ build/grunt.js
@@ -194,26 +194,16 @@ module.exports = function(grunt) {
     },
     cssmin: {
       minify: {
-        expand: true,
-        cwd: 'build/temp/',
-        src: ['video-js.css', 'alt/video-js-cdn.css'],
-        dest: 'build/temp/',
-        ext: '.min.css'
-      },
-      ie8: {
-        expand: true,
-        cwd: 'build/temp/ie8/',
-        src: ['videojs-ie8.css'],
-        dest: 'build/temp/ie8/',
-        ext: '.min.css'
+        files: {
+          'build/temp/video-js.min.css': ['build/temp/video-js.css', 'build/temp/alt/video-js-cdn.css', 'src/css/ie8.css']
+        }
       }
     },
     sass: {
       build: {
         files: {
           'build/temp/video-js.css': 'src/css/vjs.scss',
-          'build/temp/alt/video-js-cdn.css': 'src/css/vjs-cdn.scss',
-          'build/temp/ie8/videojs-ie8.css': 'src/css/ie8.scss'
+          'build/temp/alt/video-js-cdn.css': 'src/css/vjs-cdn.scss'
         }
       }
     },
@@ -402,20 +392,17 @@ module.exports = function(grunt) {
       }
     },
     concat: {
+      options: {
+        separator: '\n'
+      },
       novtt: {
-        options: {
-          separator: '\n'
-        },
         src: ['build/temp/video.js'],
         dest: 'build/temp/alt/video.novtt.js'
       },
       vtt: {
-        options: {
-          separator: '\n',
-        },
         src: ['build/temp/video.js', 'node_modules/videojs-vtt.js/dist/vtt.js'],
-        dest: 'build/temp/video.js',
-      },
+        dest: 'build/temp/video.js'
+      }
     },
     concurrent: {
       options: {
@@ -474,7 +461,6 @@ module.exports = function(grunt) {
     'sass',
     'version:css',
     'cssmin',
-    'cssmin:ie8',
 
     'copy:fonts',
     'copy:swf',
