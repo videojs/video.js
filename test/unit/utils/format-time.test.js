7e5b085a1bfa4b8f37d2be205e410850167f16a1
brandonocasey <brandonocasey@gmail.com>
2016-07-29 17:46:04 -0400

    run videojs-standard on all of the test/unit/utils files

--- test/unit/utils/format-time.test.js
+++ test/unit/utils/format-time.test.js
@@ -1,35 +1,36 @@
+/* eslint-env qunit */
 import formatTime from '../../../src/js/utils/format-time.js';
 
-q.module('format-time');
+QUnit.module('format-time');
 
-test('should format time as a string', function(){
-  ok(formatTime(1) === '0:01');
-  ok(formatTime(10) === '0:10');
-  ok(formatTime(60) === '1:00');
-  ok(formatTime(600) === '10:00');
-  ok(formatTime(3600) === '1:00:00');
-  ok(formatTime(36000) === '10:00:00');
-  ok(formatTime(360000) === '100:00:00');
+QUnit.test('should format time as a string', function() {
+  QUnit.ok(formatTime(1) === '0:01');
+  QUnit.ok(formatTime(10) === '0:10');
+  QUnit.ok(formatTime(60) === '1:00');
+  QUnit.ok(formatTime(600) === '10:00');
+  QUnit.ok(formatTime(3600) === '1:00:00');
+  QUnit.ok(formatTime(36000) === '10:00:00');
+  QUnit.ok(formatTime(360000) === '100:00:00');
 
   // Using guide should provide extra leading zeros
-  ok(formatTime(1,1) === '0:01');
-  ok(formatTime(1,10) === '0:01');
-  ok(formatTime(1,60) === '0:01');
-  ok(formatTime(1,600) === '00:01');
-  ok(formatTime(1,3600) === '0:00:01');
+  QUnit.ok(formatTime(1, 1) === '0:01');
+  QUnit.ok(formatTime(1, 10) === '0:01');
+  QUnit.ok(formatTime(1, 60) === '0:01');
+  QUnit.ok(formatTime(1, 600) === '00:01');
+  QUnit.ok(formatTime(1, 3600) === '0:00:01');
   // Don't do extra leading zeros for hours
-  ok(formatTime(1,36000) === '0:00:01');
-  ok(formatTime(1,360000) === '0:00:01');
+  QUnit.ok(formatTime(1, 36000) === '0:00:01');
+  QUnit.ok(formatTime(1, 360000) === '0:00:01');
 
   // Do not display negative time
-  ok(formatTime(-1) === '0:00');
-  ok(formatTime(-1,3600) === '0:00:00');
+  QUnit.ok(formatTime(-1) === '0:00');
+  QUnit.ok(formatTime(-1, 3600) === '0:00:00');
 });
 
-test('should format invalid times as dashes', function(){
-  equal(formatTime(Infinity, 90), '-:-');
-  equal(formatTime(NaN), '-:-');
-  // equal(formatTime(NaN, 216000), '-:--:--');
-  equal(formatTime(10, Infinity), '0:00:10');
-  equal(formatTime(90, NaN), '1:30');
+QUnit.test('should format invalid times as dashes', function() {
+  QUnit.equal(formatTime(Infinity, 90), '-:-');
+  QUnit.equal(formatTime(NaN), '-:-');
+  // QUnit.equal(formatTime(NaN, 216000), '-:--:--');
+  QUnit.equal(formatTime(10, Infinity), '0:00:10');
+  QUnit.equal(formatTime(90, NaN), '1:30');
 });
