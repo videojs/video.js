5a5ef4b82e65f20a96971e65b79f59b800b3b951
Brandon Casey <iN7h33nD@gmail.com>
2016-08-03 15:28:35 -0400

    vjsstandard - Tests - Utils folder (#3482)

--- test/unit/utils/log.test.js
+++ test/unit/utils/log.test.js
@@ -1,9 +1,11 @@
+/* eslint-env qunit */
 import {IE_VERSION} from '../../../src/js/utils/browser';
 import log from '../../../src/js/utils/log.js';
 import {logByType} from '../../../src/js/utils/log.js';
 import window from 'global/window';
+import sinon from 'sinon';
 
-q.module('log', {
+QUnit.module('log', {
 
   beforeEach() {
 
@@ -35,7 +37,7 @@ q.module('log', {
 const getConsoleArgs = (...arr) =>
   IE_VERSION && IE_VERSION < 11 ? [arr.join(' ')] : arr;
 
-test('logging functions should work', function() {
+QUnit.test('logging functions should work', function() {
 
   // Need to reset history here because there are extra messages logged
   // when running via Karma.
@@ -45,28 +47,28 @@ test('logging functions should work', function() {
   log.warn('warn1', 'warn2');
   log.error('error1', 'error2');
 
-  ok(window.console.log.called, 'log was called');
-  deepEqual(
+  QUnit.ok(window.console.log.called, 'log was called');
+  QUnit.deepEqual(
     window.console.log.firstCall.args,
     getConsoleArgs('VIDEOJS:', 'log1', 'log2')
   );
 
-  ok(window.console.warn.called, 'warn was called');
-  deepEqual(
+  QUnit.ok(window.console.warn.called, 'warn was called');
+  QUnit.deepEqual(
     window.console.warn.firstCall.args,
     getConsoleArgs('VIDEOJS:', 'WARN:', 'warn1', 'warn2')
   );
 
-  ok(window.console.error.called, 'error was called');
-  deepEqual(
+  QUnit.ok(window.console.error.called, 'error was called');
+  QUnit.deepEqual(
     window.console.error.firstCall.args,
     getConsoleArgs('VIDEOJS:', 'ERROR:', 'error1', 'error2')
   );
 
-  equal(log.history.length, 3, 'there should be three messages in the log history');
+  QUnit.equal(log.history.length, 3, 'there should be three messages in the log history');
 });
 
-test('in IE pre-11 (or when requested) objects and arrays are stringified', function() {
+QUnit.test('in IE pre-11 (or when requested) objects and arrays are stringified', function() {
 
   // Run a custom log call, explicitly requesting object/array stringification.
   logByType('log', [
@@ -78,7 +80,7 @@ test('in IE pre-11 (or when requested) objects and arrays are stringified', func
     null
   ], true);
 
-  ok(window.console.log.called, 'log was called');
-  deepEqual(window.console.log.firstCall.args,
+  QUnit.ok(window.console.log.called, 'log was called');
+  QUnit.deepEqual(window.console.log.firstCall.args,
             ['VIDEOJS: test {"foo":"bar"} [1,2,3] 0 false null']);
 });
