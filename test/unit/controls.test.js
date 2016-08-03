ebb56c95ca713a3b48004ac2b4998082e6536a24
pagarwal123 <pagarwal@brightcove.com>
2016-08-02 22:31:24 -0400

    UPDATED controls, events, menu, modal-dialog tests

--- test/unit/controls.test.js
+++ test/unit/controls.test.js
@@ -8,108 +8,115 @@ import document from 'global/document';
 
 QUnit.module('Controls');
 
-QUnit.test('should hide volume control if it\'s not supported', function(){
+QUnit.test('should hide volume control if it\'s not supported', function() {
   QUnit.expect(2);
-
-  var noop, player, volumeControl, muteToggle;
-  noop = function(){};
-  player = {
+  const noop = function() {};
+  const player = {
     id: noop,
     on: noop,
     ready: noop,
     tech_: {
-      'featuresVolumeControl': false
+      featuresVolumeControl: false
     },
-    volume: function(){},
-    muted: function(){},
-    reportUserActivity: function(){}
+    volume() {},
+    muted() {},
+    reportUserActivity() {}
   };
 
-  volumeControl = new VolumeControl(player);
-  muteToggle = new MuteToggle(player);
+  const volumeControl = new VolumeControl(player);
+  const muteToggle = new MuteToggle(player);
 
-  QUnit.ok(volumeControl.el().className.indexOf('vjs-hidden') >= 0, 'volumeControl is not hidden');
-  QUnit.ok(muteToggle.el().className.indexOf('vjs-hidden') >= 0, 'muteToggle is not hidden');
+  QUnit.ok(volumeControl.el().className.indexOf('vjs-hidden') >= 0,
+  'volumeControl is not hidden');
+  QUnit.ok(muteToggle.el().className.indexOf('vjs-hidden') >= 0,
+  'muteToggle is not hidden');
 });
 
-QUnit.test('should test and toggle volume control on `loadstart`', function(){
-  var noop, listeners, player, volumeControl, muteToggle, i;
-  noop = function(){};
-  listeners = [];
-  player = {
+QUnit.test('should test and toggle volume control on `loadstart`', function() {
+  let i;
+
+  const noop = function() {};
+  const listeners = [];
+  const player = {
     id: noop,
-    on: function(event, callback){
+    on(event, callback) {
       // don't fire dispose listeners
       if (event !== 'dispose') {
         listeners.push(callback);
       }
     },
     ready: noop,
-    volume: function(){
+    volume() {
       return 1;
     },
-    muted: function(){
+    muted() {
       return false;
     },
     tech_: {
-      'featuresVolumeControl': true
+      featuresVolumeControl: true
     },
-    reportUserActivity: function(){}
+    reportUserActivity() {}
   };
 
-  volumeControl = new VolumeControl(player);
-  muteToggle = new MuteToggle(player);
+  const volumeControl = new VolumeControl(player);
+  const muteToggle = new MuteToggle(player);
 
-  QUnit.equal(volumeControl.hasClass('vjs-hidden'), false, 'volumeControl is hidden initially');
+  QUnit.equal(volumeControl.hasClass('vjs-hidden'), false,
+  'volumeControl is hidden initially');
   QUnit.equal(muteToggle.hasClass('vjs-hidden'), false, 'muteToggle is hidden initially');
 
-  player.tech_['featuresVolumeControl'] = false;
+  player.tech_.featuresVolumeControl = false;
   for (i = 0; i < listeners.length; i++) {
     listeners[i]();
   }
 
-  QUnit.equal(volumeControl.hasClass('vjs-hidden'), true, 'volumeControl does not hide itself');
+  QUnit.equal(volumeControl.hasClass('vjs-hidden'), true,
+  'volumeControl does not hide itself');
   QUnit.equal(muteToggle.hasClass('vjs-hidden'), true, 'muteToggle does not hide itself');
 
-  player.tech_['featuresVolumeControl'] = true;
+  player.tech_.featuresVolumeControl = true;
   for (i = 0; i < listeners.length; i++) {
     listeners[i]();
   }
 
-  QUnit.equal(volumeControl.hasClass('vjs-hidden'), false, 'volumeControl does not show itself');
-  QUnit.equal(muteToggle.hasClass('vjs-hidden'), false, 'muteToggle does not show itself');
+  QUnit.equal(volumeControl.hasClass('vjs-hidden'), false,
+  'volumeControl does not show itself');
+  QUnit.equal(muteToggle.hasClass('vjs-hidden'), false,
+  'muteToggle does not show itself');
 });
 
 QUnit.test('calculateDistance should use changedTouches, if available', function() {
-  var noop, player, slider, event;
-  noop = function(){};
-  player = {
+  const noop = function() {};
+  const player = {
     id: noop,
     on: noop,
     ready: noop,
     reportUserActivity: noop
   };
-  slider = new Slider(player);
+  const slider = new Slider(player);
+
   document.body.appendChild(slider.el_);
   slider.el_.style.position = 'absolute';
   slider.el_.style.width = '200px';
   slider.el_.style.left = '0px';
 
-  event = {
+  const event = {
     pageX: 10,
     changedTouches: [{
       pageX: 100
     }]
   };
 
-  QUnit.equal(slider.calculateDistance(event), 0.5, 'we should have touched exactly in the center, so, the ratio should be half');
+  QUnit.equal(slider.calculateDistance(event), 0.5,
+  'we should have touched exactly in the center, so, the ratio should be half');
 });
 
-QUnit.test('should hide playback rate control if it\'s not supported', function(){
+QUnit.test('should hide playback rate control if it\'s not supported', function() {
   QUnit.expect(1);
 
-  var player = TestHelpers.makePlayer();
-  var playbackRate = new PlaybackRateMenuButton(player);
+  const player = TestHelpers.makePlayer();
+  const playbackRate = new PlaybackRateMenuButton(player);
 
-  QUnit.ok(playbackRate.el().className.indexOf('vjs-hidden') >= 0, 'playbackRate is not hidden');
+  QUnit.ok(playbackRate.el().className.indexOf('vjs-hidden') >= 0,
+  'playbackRate is not hidden');
 });
--- test/unit/events.test.js
+++ test/unit/events.test.js
@@ -4,25 +4,27 @@ import document from 'global/document';
 
 QUnit.module('Events');
 
-QUnit.test('should add and remove an event listener to an element', function(){
+QUnit.test('should add and remove an event listener to an element', function() {
   QUnit.expect(1);
 
-  var el = document.createElement('div');
-  var listener = function(){
+  const el = document.createElement('div');
+  const listener = function() {
     QUnit.ok(true, 'Click Triggered');
   };
 
   Events.on(el, 'click', listener);
-  Events.trigger(el, 'click'); // 1 click
+  Events.trigger(el, 'click');
+  // 1 click
   Events.off(el, 'click', listener);
-  Events.trigger(el, 'click'); // No click should happen.
+  Events.trigger(el, 'click');
+  // No click should happen.
 });
 
-QUnit.test('should add and remove multiple event listeners to an element with a single call', function(){
+QUnit.test('should add and remove multiple event listeners to an element with a single call', function() {
   QUnit.expect(6);
 
-  var el = document.createElement('div');
-  var listener = function(){
+  const el = document.createElement('div');
+  const listener = function() {
     QUnit.ok(true, 'Callback triggered');
   };
 
@@ -31,91 +33,100 @@ QUnit.test('should add and remove multiple event listeners to an element with a 
   Events.trigger(el, 'click');
   Events.trigger(el, 'click');
   Events.off(el, 'click', listener);
-  Events.trigger(el, 'click'); // No click should happen.
+  Events.trigger(el, 'click');
+  // No click should happen.
 
   Events.trigger(el, 'event1');
   Events.trigger(el, 'event1');
   Events.off(el, 'event1', listener);
-  Events.trigger(el, 'event1'); // No event1 should happen.
+  Events.trigger(el, 'event1');
+  // No event1 should happen.
 
   Events.trigger(el, 'event2');
   Events.trigger(el, 'event2');
   Events.off(el, 'event2', listener);
-  Events.trigger(el, 'event2'); // No event2 should happen.
+  Events.trigger(el, 'event2');
+  // No event2 should happen.
 });
 
-QUnit.test('should be possible to pass data when you trigger an event', function () {
+QUnit.test('should be possible to pass data when you trigger an event', function() {
   QUnit.expect(6);
-  var el = document.createElement('div');
-  var fakeData1 = 'Fake Data 1';
-  var fakeData2 = {txt: 'Fake Data 2'};
+  const el = document.createElement('div');
+  const fakeData1 = 'Fake Data 1';
+  const fakeData2 = {txt: 'Fake Data 2'};
 
-  var listener = function(evt, hash){
+  const listener = function(evt, hash) {
     QUnit.ok(true, 'Callback triggered');
     deepEqual(fakeData1, hash.d1, 'Shoulbe be passed to the handler');
     deepEqual(fakeData2, hash.d2, 'Shoulbe be passed to the handler');
   };
 
   Events.on(el, ['event1', 'event2'], listener);
-  Events.trigger(el, 'event1', { d1: fakeData1, d2:fakeData2});
-  Events.trigger(el, 'event2', { d1: fakeData1, d2:fakeData2});
+  Events.trigger(el, 'event1', { d1: fakeData1, d2: fakeData2});
+  Events.trigger(el, 'event2', { d1: fakeData1, d2: fakeData2});
 
 });
 
-QUnit.test('should remove all listeners of a type', function(){
-  var el = document.createElement('div');
-  var clicks = 0;
-  var listener = function(){
+QUnit.test('should remove all listeners of a type', function() {
+  const el = document.createElement('div');
+  let clicks = 0;
+  const listener = function() {
     clicks++;
   };
-  var listener2 = function(){
+  const listener2 = function() {
     clicks++;
   };
 
   Events.on(el, 'click', listener);
   Events.on(el, 'click', listener2);
-  Events.trigger(el, 'click'); // 2 clicks
+  Events.trigger(el, 'click');
+  // 2 clicks
 
   QUnit.ok(clicks === 2, 'both click listeners fired');
 
   Events.off(el, 'click');
-  Events.trigger(el, 'click'); // No click should happen.
+  Events.trigger(el, 'click');
+  // No click should happen.
 
   QUnit.ok(clicks === 2, 'no click listeners fired');
 });
 
-QUnit.test('should remove all listeners of an array of types', function(){
-  var el = document.createElement('div');
-  var calls = 0;
-  var listener = function(){
+QUnit.test('should remove all listeners of an array of types', function() {
+  const el = document.createElement('div');
+  let calls = 0;
+  const listener = function() {
     calls++;
   };
-  var listener2 = function(){
+  const listener2 = function() {
     calls++;
   };
 
   Events.on(el, ['click', 'event1'], listener);
   Events.on(el, ['click', 'event1'], listener2);
-  Events.trigger(el, 'click'); // 2 calls
-  Events.trigger(el, 'event1'); // 2 calls
+  Events.trigger(el, 'click');
+  // 2 calls
+  Events.trigger(el, 'event1');
+  // 2 calls
 
   QUnit.ok(calls === 4, 'both click listeners fired');
 
   Events.off(el, ['click', 'event1']);
-  Events.trigger(el, 'click'); // No click should happen.
-  Events.trigger(el, 'event1'); // No event1 should happen.
+  Events.trigger(el, 'click');
+  // No click should happen.
+  Events.trigger(el, 'event1');
+  // No event1 should happen.
 
   QUnit.ok(calls === 4, 'no event listeners fired');
 });
 
-QUnit.test('should remove all listeners from an element', function(){
+QUnit.test('should remove all listeners from an element', function() {
   QUnit.expect(2);
 
-  var el = document.createElement('div');
-  var listener = function(){
+  const el = document.createElement('div');
+  const listener = function() {
     QUnit.ok(true, 'Fake1 Triggered');
   };
-  var listener2 = function(){
+  const listener2 = function() {
     QUnit.ok(true, 'Fake2 Triggered');
   };
 
@@ -132,90 +143,99 @@ QUnit.test('should remove all listeners from an element', function(){
   Events.trigger(el, 'fake2');
 });
 
-QUnit.test('should listen only once', function(){
+QUnit.test('should listen only once', function() {
   QUnit.expect(1);
 
-  var el = document.createElement('div');
-  var listener = function(){
+  const el = document.createElement('div');
+  const listener = function() {
     QUnit.ok(true, 'Click Triggered');
   };
 
   Events.one(el, 'click', listener);
-  Events.trigger(el, 'click'); // 1 click
-  Events.trigger(el, 'click'); // No click should happen.
+  Events.trigger(el, 'click');
+  // 1 click
+  Events.trigger(el, 'click');
+  // No click should happen.
 });
 
-QUnit.test( 'should listen only once in multiple events from a single call', function(){
+QUnit.test('should listen only once in multiple events from a single call', function() {
   QUnit.expect(3);
 
-  var el = document.createElement('div');
-  var listener = function(){
+  const el = document.createElement('div');
+  const listener = function() {
     QUnit.ok(true, 'Callback Triggered');
   };
 
   Events.one(el, ['click', 'event1', 'event2'], listener);
-  Events.trigger(el, 'click'); // 1 click
-  Events.trigger(el, 'click'); // No click should happen.
-  Events.trigger(el, 'event1'); // event1 must be handled.
-  Events.trigger(el, 'event1'); // No event1 should be handled.
-  Events.trigger(el, 'event2'); // event2 must be handled.
-  Events.trigger(el, 'event2'); // No event2 should be handled.
+  Events.trigger(el, 'click');
+  // 1 click
+  Events.trigger(el, 'click');
+  // No click should happen.
+  Events.trigger(el, 'event1');
+  // event1 must be handled.
+  Events.trigger(el, 'event1');
+  // No event1 should be handled.
+  Events.trigger(el, 'event2');
+  // event2 must be handled.
+  Events.trigger(el, 'event2');
+  // No event2 should be handled.
 });
 
-QUnit.test('should stop immediate propagtion', function(){
+QUnit.test('should stop immediate propagtion', function() {
   QUnit.expect(1);
 
-  var el = document.createElement('div');
+  const el = document.createElement('div');
 
-  Events.on(el, 'test', function(e){
+  Events.on(el, 'test', function(e) {
     QUnit.ok(true, 'First listener fired');
     e.stopImmediatePropagation();
   });
 
-  Events.on(el, 'test', function(e){
+  Events.on(el, 'test', function(e) {
     QUnit.ok(false, 'Second listener fired');
   });
 
   Events.trigger(el, 'test');
 });
 
-QUnit.test('should bubble up DOM unless bubbles == false', function(){
+QUnit.test('should bubble up DOM unless bubbles == false', function() {
   QUnit.expect(3);
 
-  var outer = document.createElement('div');
-  var inner = outer.appendChild(document.createElement('div'));
+  const outer = document.createElement('div');
+  const inner = outer.appendChild(document.createElement('div'));
 
   // Verify that if bubbles === true, event bubbles up dom.
-  Events.on(inner, 'bubbles', function(e){
+  Events.on(inner, 'bubbles', function(e) {
     QUnit.ok(true, 'Inner listener fired');
   });
-  Events.on(outer, 'bubbles', function(e){
+  Events.on(outer, 'bubbles', function(e) {
     QUnit.ok(true, 'Outer listener fired');
   });
-  Events.trigger(inner, { type:'bubbles', target:inner, bubbles:true });
+  Events.trigger(inner, { type: 'bubbles', target: inner, bubbles: true });
 
   // Only change 'bubbles' to false, and verify only inner handler is called.
-  Events.on(inner, 'nobub', function(e){
+  Events.on(inner, 'nobub', function(e) {
     QUnit.ok(true, 'Inner listener fired');
   });
-  Events.on(outer, 'nobub', function(e){
+  Events.on(outer, 'nobub', function(e) {
     QUnit.ok(false, 'Outer listener fired');
   });
-  Events.trigger(inner, { type:'nobub', target:inner, bubbles:false });
+  Events.trigger(inner, { type: 'nobub', target: inner, bubbles: false });
 });
 
 QUnit.test('should have a defaultPrevented property on an event that was prevent from doing default action', function() {
   QUnit.expect(2);
 
-  var el = document.createElement('div');
+  const el = document.createElement('div');
 
-  Events.on(el, 'test', function(e){
+  Events.on(el, 'test', function(e) {
     QUnit.ok(true, 'First listener fired');
     e.preventDefault();
   });
 
-  Events.on(el, 'test', function(e){
-    QUnit.ok(e.defaultPrevented, 'Should have `defaultPrevented` to signify preventDefault being called');
+  Events.on(el, 'test', function(e) {
+    QUnit.ok(e.defaultPrevented,
+      'Should have `defaultPrevented` to signify preventDefault being called');
   });
 
   Events.trigger(el, 'test');
@@ -224,19 +244,20 @@ QUnit.test('should have a defaultPrevented property on an event that was prevent
 QUnit.test('should have relatedTarget correctly set on the event', function() {
   QUnit.expect(2);
 
-  var el1 = document.createElement('div'),
-      el2 = document.createElement('div'),
-      relatedEl = document.createElement('div');
+  const el1 = document.createElement('div');
+  const el2 = document.createElement('div');
+  const relatedEl = document.createElement('div');
 
-  Events.on(el1, 'click', function(e){
-    QUnit.equal(e.relatedTarget, relatedEl, 'relatedTarget is set for all browsers when related element is set on the event');
+  Events.on(el1, 'click', function(e) {
+    QUnit.equal(e.relatedTarget, relatedEl,
+      'relatedTarget is set for all browsers when related element is set on the event');
   });
 
-  Events.trigger(el1, { type:'click', relatedTarget:relatedEl });
+  Events.trigger(el1, { type: 'click', relatedTarget: relatedEl });
 
   Events.on(el2, 'click', function(e) {
     QUnit.equal(e.relatedTarget, null, 'relatedTarget is null when none is provided');
   });
 
-  Events.trigger(el2, { type:'click', relatedTarget:undefined });
+  Events.trigger(el2, { type: 'click', relatedTarget: undefined });
 });
