c62de28e3ba97cb8579ae5ac616e368c370baaf6
Gary Katsevman <git@gkatsev.com>
2016-10-03 16:57:15 -0400

    feat: add a safe getComputedStyle to videojs.
    This is used internally in the seek bar and mouse time display.
    In firefox, in an iframe that is hidden with "display: none",
    getComputedStyle() returns "null" which can break things.
    See https://bugzilla.mozilla.org/show_bug.cgi?id=548397 for more
    details.

--- src/js/video.js
+++ src/js/video.js
@@ -25,6 +25,7 @@ import log from './utils/log.js';
 import * as Dom from './utils/dom.js';
 import * as browser from './utils/browser.js';
 import * as Url from './utils/url.js';
+import computedStyle from './utils/get-computed-style.js';
 import extendFn from './extend.js';
 import merge from 'lodash-compat/object/merge';
 import xhr from 'xhr';
@@ -718,6 +719,20 @@ videojs.appendContent = Dom.appendContent;
  * @return {Element}
  */
 videojs.insertContent = Dom.insertContent;
+
+/**
+ * A safe getComputedStyle with an IE8 fallback.
+ *
+ * This is because in Firefox, if the player is loaded in an iframe with `display:none`,
+ * then `getComputedStyle` returns `null`, so, we do a null-check to make sure
+ * that the player doesn't break in these cases.
+ * See https://bugzilla.mozilla.org/show_bug.cgi?id=548397 for more details.
+ *
+ * @function getComputedStyle
+ * @param el the element you want the computed style of
+ * @param prop the property name you want
+ */
+videojs.getComputedStyle = computedStyle;
 
 /*
  * Custom Universal Module Definition (UMD)
