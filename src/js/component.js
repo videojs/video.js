668f6e8716bda21656e8ab4a6795a1f2f13a42f3
Gary Katsevman <git@gkatsev.com>
2016-11-23 14:49:33 -0500

    window

--- src/js/component.js
+++ src/js/component.js
@@ -1441,13 +1441,13 @@ class Component {
     }
 
     if (window.foo) {
-      console.log(name, Component.components_[name]);
+      window.console.log(name, Component.components_[name]);
     }
     if (name === 'Player' && Component.components_[name]) {
       const Player = Component.components_[name];
 
       if (window.foo) {
-        console.log(Object.keys(Player.players));
+        window.console.log(Object.keys(Player.players));
       }
       if (Player.players && Object.keys(Player.players).length > 0) {
         throw new Error('Can not register Player component after player has been created');
