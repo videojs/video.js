83cbeec424fc3537524621284a57b4b6c7a157b4
Gary Katsevman <git@gkatsev.com>
2016-12-14 13:21:35 -0500

    fix: remove vjs-seeking on src change (#3846)
    If you're in a seeking state and the source changed, we should no longer
    be in a seeking state.
    
    Fixes #3765.

--- src/js/player.js
+++ src/js/player.js
@@ -1077,6 +1077,7 @@ class Player extends Component {
     // TODO: Update to use `emptied` event instead. See #1277.
 
     this.removeClass('vjs-ended');
+    this.removeClass('vjs-seeking');
 
     // reset the error state
     this.error(null);
