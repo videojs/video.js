22cf3dd935de58eb93205dd9a5d285bf24970976
Brandon Casey <BrandonOCasey@users.noreply.github.com>
2016-12-21 14:23:00 -0500

    feat: deprecate the use of `starttime` in player.js (#3838)

--- src/js/player.js
+++ src/js/player.js
@@ -1250,12 +1250,14 @@ class Player extends Component {
    *
    * @fires Player#firstplay
    * @listens Tech#firstplay
+   * @deprecated As of 6.0 passing the `starttime` option to the player will be deprecated
    * @private
    */
   handleTechFirstPlay_() {
     // If the first starttime attribute is specified
     // then we will start at the given offset in seconds
     if (this.options_.starttime) {
+      log.warn('Passing the `starttime` option to the player will be deprecated in 6.0');
       this.currentTime(this.options_.starttime);
     }
 
