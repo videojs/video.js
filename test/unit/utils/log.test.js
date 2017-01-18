844e4f0107b2d7ee2f392cfb9aaa0ae71a607b3e
Pat O'Neill <pgoneill@gmail.com>
2017-01-18 00:35:42 -0500

    feat: Log Levels (#3853)
    Add a log levels and history api: `videojs.log.level()` and `videojs.log.history()`.
    `.level()` will return the current level and you can also set it to be one of: `all`, `error`, `off`, or `warn`.
    `.history()` will return a list of all things logged since videojs loaded. It can be disabled via `videojs.log.history.disable()` (and re-enabled with `enable()`) as well as cleared with `videojs.log.history.clear()`.

--- test/unit/utils/log.test.js
+++ test/unit/utils/log.test.js
@@ -5,7 +5,7 @@ import {logByType} from '../../../src/js/utils/log.js';
 import window from 'global/window';
 import sinon from 'sinon';
 
-QUnit.module('log', {
+QUnit.module('utils/log', {
 
   beforeEach() {
 
@@ -29,8 +29,11 @@ QUnit.module('log', {
     // Restore the native/original console.
     window.console = this.originalConsole;
 
+    // Restore the default logging level.
+    log.level(log.levels.DEFAULT);
+
     // Empty the logger's history.
-    log.history.length = 0;
+    log.history.clear();
   }
 });
 
@@ -41,7 +44,7 @@ QUnit.test('logging functions should work', function(assert) {
 
   // Need to reset history here because there are extra messages logged
   // when running via Karma.
-  log.history.length = 0;
+  log.history.clear();
 
   log('log1', 'log2');
   log.warn('warn1', 'warn2');
@@ -65,10 +68,19 @@ QUnit.test('logging functions should work', function(assert) {
     getConsoleArgs('VIDEOJS:', 'ERROR:', 'error1', 'error2')
   );
 
-  assert.equal(log.history.length, 3, 'there should be three messages in the log history');
+  const history = log.history();
+
+  assert.equal(history.length, 3, 'there should be three messages in the log history');
+  assert.deepEqual(history[0], ['log1', 'log2'], 'history recorded the correct arguments');
+  assert.deepEqual(history[1], ['WARN:', 'warn1', 'warn2'], 'history recorded the correct arguments');
+  assert.deepEqual(history[2], ['ERROR:', 'error1', 'error2'], 'history recorded the correct arguments');
 });
 
 QUnit.test('in IE pre-11 (or when requested) objects and arrays are stringified', function(assert) {
+
+  // Need to reset history here because there are extra messages logged
+  // when running via Karma.
+  log.history.clear();
 
   // Run a custom log call, explicitly requesting object/array stringification.
   logByType('log', [
@@ -83,4 +95,68 @@ QUnit.test('in IE pre-11 (or when requested) objects and arrays are stringified'
   assert.ok(window.console.log.called, 'log was called');
   assert.deepEqual(window.console.log.firstCall.args,
             ['VIDEOJS: test {"foo":"bar"} [1,2,3] 0 false null']);
+});
+
+QUnit.test('setting the log level changes what is actually logged', function(assert) {
+
+  // Need to reset history here because there are extra messages logged
+  // when running via Karma.
+  log.history.clear();
+
+  log.level('error');
+
+  log('log1', 'log2');
+  log.warn('warn1', 'warn2');
+  log.error('error1', 'error2');
+
+  assert.notOk(window.console.log.called, 'console.log was not called');
+  assert.notOk(window.console.warn.called, 'console.warn was not called');
+  assert.ok(window.console.error.called, 'console.error was called');
+
+  const history = log.history();
+
+  assert.deepEqual(history[0], ['log1', 'log2'], 'history is maintained even when logging is not performed');
+  assert.deepEqual(history[1], ['WARN:', 'warn1', 'warn2'], 'history is maintained even when logging is not performed');
+  assert.deepEqual(history[2], ['ERROR:', 'error1', 'error2'], 'history is maintained even when logging is not performed');
+
+  log.level('off');
+
+  log('log1', 'log2');
+  log.warn('warn1', 'warn2');
+  log.error('error1', 'error2');
+
+  assert.notOk(window.console.log.called, 'console.log was not called');
+  assert.notOk(window.console.warn.called, 'console.warn was not called');
+  assert.strictEqual(window.console.error.callCount, 1, 'console.error was not called again');
+
+  assert.throws(
+    () => log.level('foobar'),
+    new Error('"foobar" in not a valid log level'),
+    'log.level() only accepts valid log levels when used as a setter'
+  );
+});
+
+QUnit.test('history can be enabled/disabled', function(assert) {
+
+  // Need to reset history here because there are extra messages logged
+  // when running via Karma.
+  log.history.clear();
+
+  log.history.disable();
+  log('log1');
+  log.warn('warn1');
+  log.error('error1');
+
+  let history = log.history();
+
+  assert.strictEqual(history.length, 0, 'no history was tracked');
+
+  log.history.enable();
+  log('log1');
+  log.warn('warn1');
+  log.error('error1');
+
+  history = log.history();
+
+  assert.strictEqual(history.length, 3, 'history was tracked');
 });
