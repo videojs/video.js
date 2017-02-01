7a14425a36f2ddf099ddd2ef7d1f094cdfc6845e
Gary Katsevman <git@gkatsev.com>
2017-02-01 17:09:15 -0500

    localise TextTrackCue

--- src/js/tracks/text-track-cue-list.js
+++ src/js/tracks/text-track-cue-list.js
@@ -5,7 +5,7 @@ import * as browser from '../utils/browser.js';
 import document from 'global/document';
 
 /**
- * @typedef {Object} TextTrackCue
+ * @typedef {Object} TextTrackCueList~TextTrackCue
  *
  * @property {string} id
  *           The unique id for this text track cue
@@ -109,7 +109,7 @@ class TextTrackCueList {
    * @param {string} id
    *        The id of the cue that should be searched for.
    *
-   * @return {TextTrackCue|null}
+   * @return {TextTrackCueList~TextTrackCue|null}
    *         A single cue or null if none was found.
    */
   getCueById(id) {
