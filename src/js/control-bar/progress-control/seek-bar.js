db0112053fc152f5dfe1b5b2e1cba7e3882a426e
Gary Katsevman <git@gkatsev.com>
2017-02-21 16:17:00 -0500

    feat: allow tokens in localize, localize progress bar time (#4060)
    It allows you to use string as an key for the language files but have a default value that's different. In this case, we have a key of progress bar timing: currentTime={1} duration={2} which is used so that we have a descriptive key to what is happening. The default value matches that default value in en.json: {1} of {2}. The reason for the default value is so that the translation uses {1} of {2} for the actual localization without requiring en.json to be included in videojs by default.
    
    Fix #4024.

--- src/js/control-bar/progress-control/seek-bar.js
+++ src/js/control-bar/progress-control/seek-bar.js
@@ -74,7 +74,11 @@ class SeekBar extends Slider {
     this.el_.setAttribute('aria-valuenow', (percent * 100).toFixed(2));
 
     // human readable value of progress bar (time complete)
-    this.el_.setAttribute('aria-valuetext', formatTime(time, duration));
+    this.el_.setAttribute('aria-valuetext',
+                          this.localize('progress bar timing: currentTime={1} duration={2}',
+                                        [formatTime(time, duration),
+                                         formatTime(duration, duration)],
+                                        '{1} of {2}'));
 
     // Update the `PlayProgressBar`.
     this.bar.update(Dom.getBoundingClientRect(this.el_), percent);
