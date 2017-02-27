4a55778aa6ef4d6a86e13e8601e9c900c9b99b7e
Gary Katsevman <git@gkatsev.com>
2017-02-27 18:57:51 -0500

    re-add browsers

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -114,13 +114,13 @@ module.exports = function(config) {
   if (process.env.TRAVIS) {
     if (process.env.BROWSER_STACK_USERNAME) {
       settings.browsers = [
-        // 'chrome_bs',
-        // 'firefox_bs',
-        // 'safari_bs',
-        // 'edge_bs',
+        'chrome_bs',
+        'firefox_bs',
+        'safari_bs',
+        'edge_bs',
         'ie11_bs',
-        // 'ie10_bs',
-        // 'ie9_bs',
+        'ie10_bs',
+        'ie9_bs',
         'ie8_bs'
       ];
     } else {
