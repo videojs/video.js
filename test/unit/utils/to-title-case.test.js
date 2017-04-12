9dddb33bd58edb84e2f37b440b3034decfdc9a21
Gary Katsevman <git@gkatsev.com>
2017-04-12 17:02:38 -0400

    negative test cases for titleCaseEquals

--- test/unit/utils/to-title-case.test.js
+++ test/unit/utils/to-title-case.test.js
@@ -14,4 +14,9 @@ QUnit.test('titleCaseEquals compares whether the TitleCase of two strings is equ
   assert.ok(titleCaseEquals('foo', 'Foo'), 'foo equals Foo');
   assert.ok(titleCaseEquals('Foo', 'foo'), 'Foo equals foo');
   assert.ok(titleCaseEquals('Foo', 'Foo'), 'Foo equals Foo');
+
+  assert.ok(titleCaseEquals('fooBar', 'fooBar'), 'fooBar equals fooBar');
+  assert.notOk(titleCaseEquals('fooBAR', 'fooBar'), 'fooBAR does not equal fooBar');
+  assert.notOk(titleCaseEquals('foobar', 'fooBar'), 'foobar does not equal fooBar');
+  assert.notOk(titleCaseEquals('fooBar', 'FOOBAR'), 'fooBar does not equal fooBAR');
 });
