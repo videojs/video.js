4b65a58690aefb0b9d1c93c82150b251f043e61a
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2017-06-05 14:39:29 -0400

    fix IE10 issue for disableOthers when property access results in "permission denied"
    issue #4378

--- src/js/tracks/audio-track-list.js
+++ src/js/tracks/audio-track-list.js
@@ -19,7 +19,7 @@ import document from 'global/document';
  */
 const disableOthers = function(list, track) {
   for (let i = 0; i < list.length; i++) {
-    if (track.id === list[i].id) {
+    if (!Object.keys(list[i]).length || track.id === list[i].id) {
       continue;
     }
     // another audio track is enabled, disable it
