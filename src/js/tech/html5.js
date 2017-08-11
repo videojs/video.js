109c06332756ab864f5c5ceaaa82b7296b485448
Gary Katsevman <git@gkatsev.com>
2017-08-11 15:29:22 -0400

    fix linting again

--- src/js/tech/html5.js
+++ src/js/tech/html5.js
@@ -233,7 +233,6 @@ class Html5 extends Tech {
       el.playerId = this.options_.playerId;
     }
 
-
     if (this.options_.preload !== 'undefined') {
       Dom.setAttribute(el, 'preload', this.options_.preload);
     }
