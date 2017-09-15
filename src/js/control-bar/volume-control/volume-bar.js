f5094c348ea47d4de3d8c69b909f3e488404c707
Chuong <kocoten1992@users.noreply.github.com>
2017-09-15 22:13:20 +0700

    handle touchscreen on ios

--- src/js/control-bar/volume-control/volume-bar.js
+++ src/js/control-bar/volume-control/volume-bar.js
@@ -57,6 +57,8 @@ class VolumeBar extends Slider {
     // only allow left-click volume change
     if (typeof event.button === 'undefined' && typeof event.buttons === 'undefined') {
       // touch screen, not apply, do nothing
+    } else if (event.button === 0 && typeof event.buttons === 'undefined') {
+      // touch screen, safari on ios
     } else if (event.button !== 0 || event.buttons !== 1) {
       return;
     }
