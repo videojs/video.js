f51d36b053e5e47beebeb91d5e7b3ec1e269b197
Brandon Casey <BrandonOCasey@users.noreply.github.com>
2017-10-02 11:19:29 -0400

    feat: display currentTime as duration and remainingTime as 0 on ended (#4634)

--- src/js/control-bar/time-controls/current-time-display.js
+++ src/js/control-bar/time-controls/current-time-display.js
@@ -12,6 +12,20 @@ import Component from '../../component.js';
 class CurrentTimeDisplay extends TimeDisplay {
 
   /**
+   * Creates an instance of this class.
+   *
+   * @param {Player} player
+   *        The `Player` that this class should be attached to.
+   *
+   * @param {Object} [options]
+   *        The key/value store of player options.
+   */
+  constructor(player, options) {
+    super(player, options);
+    this.on(player, 'ended', this.handleEnded);
+  }
+
+  /**
    * Builds the default DOM `className`.
    *
    * @return {string}
@@ -34,6 +48,23 @@ class CurrentTimeDisplay extends TimeDisplay {
     const time = (this.player_.scrubbing()) ? this.player_.getCache().currentTime : this.player_.currentTime();
 
     this.updateFormattedTime_(time);
+  }
+
+  /**
+   * When the player fires ended there should be no time left. Sadly
+   * this is not always the case, lets make it seem like that is the case
+   * for users.
+   *
+   * @param {EventTarget~Event} [event]
+   *        The `ended` event that caused this to run.
+   *
+   * @listens Player#ended
+   */
+  handleEnded(event) {
+    if (!this.player_.duration()) {
+      return;
+    }
+    this.updateFormattedTime_(this.player.duration());
   }
 
 }
