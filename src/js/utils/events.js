8567e031f67b9d30e8b5b0831922c5b80638fcbc
Marc A. Modrow <mmodrow@uni-bremen.de>
2017-10-18 20:47:29 +0200

    Implemented feedback from @gkatsev .

--- src/js/utils/events.js
+++ src/js/utils/events.js
@@ -351,11 +351,9 @@ export function off(elem, type, fn) {
 
   // Are we removing all bound events?
   if (!type) {
-    if (!!data && !!data.handlers) {
-      for (const t in data.handlers) {
-        if (!!data.handlers && !!data.handlers.hasOwnProperty && data.handlers.hasOwnProperty(t)) {
-          removeType(t);
-        }
+    for (const t in data.handlers) {
+      if (data.handlers) {
+        removeType(t);
       }
     }
     return;
