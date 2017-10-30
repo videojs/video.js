79639138ba41eda75f0f6a07071f9a9e6a551f80
Marc A. Modrow <mmodrow@uni-bremen.de>
2017-10-30 23:06:41 +0100

    fix: Events#off threw if Object.prototype had extra enumerable properties, don't remove all events if off receives a falsey value (#4669)
    If Object.prototype was modified to have extra properties, the `off` method crashed because `data.handlers` would've been removed before getting to the extra property.
    
    Also, this fixes an issue where if a falsey value was passed to off, the events system would remove all the events, i.e. `player.off('')`. Instead, we make sure that only if it is called as `player.off()` will they be removed.

--- src/js/utils/events.js
+++ src/js/utils/events.js
@@ -350,9 +350,11 @@ export function off(elem, type, fn) {
   };
 
   // Are we removing all bound events?
-  if (!type) {
+  if (type === undefined) {
     for (const t in data.handlers) {
-      removeType(t);
+      if (Object.prototype.hasOwnProperty.call(data.handlers || {}, t)) {
+        removeType(t);
+      }
     }
     return;
   }
