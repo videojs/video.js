a3148e78d0e5b94bc1093beb0b5c687c550e8cd5
Gary Katsevman <git@gkatsev.com>
2017-11-29 16:56:16 -0500

    error build on master if not tagged commit

--- build/netlify-docs.js
+++ build/netlify-docs.js
@@ -1,16 +1,15 @@
 const sh = require('shelljs');
 
 const GIT_CONTAINS = `git tag --contains ${process.env.COMMIT_REF}`;
-const DOCS = 'npm run docs:api';
 
 const output = sh.exec(GIT_CONTAINS, {async: false, silent:true}).stdout;
 
-// if we're on master branch, only generate docs if we're on a tagged commit
-// if we're on any other branch, we can regenerate docs
-if (process.env.BRANCH === 'master' && output !== '' ||
-    process.env.BRANCH !== 'master') {
-  // sh.exec(DOCS);
-}
+// if we're on master branch and not on a tagged commit,
+// error the build so it doesn't redeploy the docs
+if (process.env.BRANCH === 'master' && output === '')
+  process.exit(1);
 
-sh.rm('-rf', ['docs/api']);
-sh.mkdir('-p', 'docs/api');
+// if we're on any other branch, we can regenerate docs
+else {
+  sh.exec('npm run docs:api');
+}
