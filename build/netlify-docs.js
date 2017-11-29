063dc1c6bad3e0dabdd7ca4331590b46a339fa44
Gary Katsevman <git@gkatsev.com>
2017-11-29 16:45:24 -0500

    netlify build

---
+++ build/netlify-docs.js
@@ -0,0 +1,13 @@
+const sh = require('shelljs');
+
+const GIT_CONTAINS = `git tag --contains ${process.env.COMMIT_REF}`;
+const DOCS = 'npm run docs:api';
+
+const output = sh.exec(GIT_CONTAINS, {async: false, silent:true}).stdout;
+
+// if we're on master branch, only generate docs if we're on a tagged commit
+// if we're on any other branch, we can regenerate docs
+if (process.env.BRANCH === 'master' && output !== '' ||
+    process.env.BRANCH !== 'master') {
+  sh.exec(DOCS);
+}
