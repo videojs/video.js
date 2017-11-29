a7f15aee4e766d46cdcc90a2d30da01d4e80c51a
Gary Katsevman <git@gkatsev.com>
2017-11-29 16:29:37 -0500

    remove debug info

--- build/prepare-docs.js
+++ build/prepare-docs.js
@@ -8,7 +8,3 @@ sh.mkdir('-p', 'docs/api');
 
 // copy the legacy docs over
 sh.cp('-R', 'docs/legacy-docs', 'docs/api/docs');
-
-['REPOSITORY_URL', 'BRANCH', 'PULL_REQUEST', 'HEAD', 'COMMIT_REF', 'CONTEXT'].forEach(function(e) {
-  console.log(e, ': ', process.env[e]);
-});
