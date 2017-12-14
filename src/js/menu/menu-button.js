210db29700eb2d0b2dfbe1a41a70fe50a84e3c65
Gary Katsevman <git@gkatsev.com>
2017-12-14 13:12:36 -0500

    Fix lint warning

--- src/js/menu/menu-button.js
+++ src/js/menu/menu-button.js
@@ -345,7 +345,8 @@ class MenuButton extends Component {
       // set the focus into the submenu, except on iOS where it is resulting in
       // undesired scrolling behavior when the player is in an iframe
       if (IS_IOS && Dom.isInFrame()) {
-        return; //Return early so that the menu isn't focused
+         // Return early so that the menu isn't focused
+        return;
       }
 
       this.menu.focus();
