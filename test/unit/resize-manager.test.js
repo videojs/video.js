b9c5967ec2fe3b89a0051e2d6eb8836bf2af4cad
Gary Katsevman <git@gkatsev.com>
2018-01-10 16:21:48 -0500

    lint

--- test/unit/resize-manager.test.js
+++ test/unit/resize-manager.test.js
@@ -5,69 +5,70 @@ import * as browser from '../../src/js/utils/browser.js';
 
 if (!browser.IS_IE8) {
 
-QUnit.module('ResizeManager', {
-  beforeEach() {
-    this.player = TestHelpers.makePlayer();
-  },
-  afterEach() {
-    this.player.dispose();
-  }
-});
-
-QUnit.test('ResizeManager creates an iframe if ResizeObserver is not available', function(assert) {
-  const rm = new ResizeManager(this.player, {ResizeObserver: null});
-
-  assert.equal(rm.el().tagName.toLowerCase(), 'iframe', 'we got an iframe');
-
-  rm.dispose();
-});
-
-QUnit.test('ResizeManager uses the ResizeObserver, if given', function(assert) {
-  let roCreated = false;
-  let observeCalled = false;
-  class MyResizeObserver {
-    constructor(fn) {
-      roCreated = true;
-      this.observer = fn;
+  QUnit.module('ResizeManager', {
+    beforeEach() {
+      this.player = TestHelpers.makePlayer();
+    },
+    afterEach() {
+      this.player.dispose();
     }
-
-    observe(el) {
-      observeCalled = true;
-      this.el = el
-    }
-  };
-
-  const rm = new ResizeManager(this.player, {ResizeObserver: MyResizeObserver});
-
-  assert.ok(roCreated, 'we intantiated the RO that was passed');
-  assert.ok(observeCalled, 'we observed the RO');
-  assert.equal(rm.resizeObserver.el, this.player.el(), 'we observed the player el');
-
-  rm.dispose();
-});
-
-QUnit.test('ResizeManager triggers `playerresize` when the observer method is called', function(assert) {
-  class MyResizeObserver {
-    constructor(fn) {
-      this.observer = fn;
-    }
-
-    observe(el) {
-      this.el = el
-    }
-  };
-
-  let playerresizeCalled = 0;
-  const rm = new ResizeManager(this.player, {ResizeObserver: MyResizeObserver});
-
-  this.player.on('playerresize', function() {
-    playerresizeCalled++;
   });
-  rm.resizeObserver.observer();
 
-  assert.equal(playerresizeCalled, 1, 'playerresize was triggered');
+  QUnit.test('ResizeManager creates an iframe if ResizeObserver is not available', function(assert) {
+    const rm = new ResizeManager(this.player, {ResizeObserver: null});
 
-  rm.dispose();
-});
+    assert.equal(rm.el().tagName.toLowerCase(), 'iframe', 'we got an iframe');
+
+    rm.dispose();
+  });
+
+  QUnit.test('ResizeManager uses the ResizeObserver, if given', function(assert) {
+    let roCreated = false;
+    let observeCalled = false;
+
+    class MyResizeObserver {
+      constructor(fn) {
+        roCreated = true;
+        this.observer = fn;
+      }
+
+      observe(el) {
+        observeCalled = true;
+        this.el = el;
+      }
+    }
+
+    const rm = new ResizeManager(this.player, {ResizeObserver: MyResizeObserver});
+
+    assert.ok(roCreated, 'we intantiated the RO that was passed');
+    assert.ok(observeCalled, 'we observed the RO');
+    assert.equal(rm.resizeObserver.el, this.player.el(), 'we observed the player el');
+
+    rm.dispose();
+  });
+
+  QUnit.test('ResizeManager triggers `playerresize` when the observer method is called', function(assert) {
+    class MyResizeObserver {
+      constructor(fn) {
+        this.observer = fn;
+      }
+
+      observe(el) {
+        this.el = el;
+      }
+    }
+
+    let playerresizeCalled = 0;
+    const rm = new ResizeManager(this.player, {ResizeObserver: MyResizeObserver});
+
+    this.player.on('playerresize', function() {
+      playerresizeCalled++;
+    });
+    rm.resizeObserver.observer();
+
+    assert.equal(playerresizeCalled, 1, 'playerresize was triggered');
+
+    rm.dispose();
+  });
 
 }
