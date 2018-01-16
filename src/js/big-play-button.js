5fabef39599512e5a1f376e1afa8a10f3dc508d2
ldayananda <lahiru07@gmail.com>
2018-01-16 17:50:40 -0500

    make sure there is a play promise

--- src/js/big-play-button.js
+++ src/js/big-play-button.js
@@ -59,7 +59,7 @@ class BigPlayButton extends Button {
 
     const playFocus = () => playToggle.focus();
 
-    if (isPromise(playPromise)) {
+    if (playPromise && isPromise(playPromise)) {
       playPromise.then(playFocus, () => {});
     } else {
       this.setTimeout(playFocus, 1);
