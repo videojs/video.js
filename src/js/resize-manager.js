5cb16a3b9299cffabc04f231430541397f7541b2
Gary Katsevman <git@gkatsev.com>
2018-01-17 14:58:31 -0500

    add jsdoc to resize manager

--- src/js/resize-manager.js
+++ src/js/resize-manager.js
@@ -7,7 +7,37 @@ import * as Events from './utils/events.js';
 import mergeOptions from './utils/merge-options.js';
 import Component from './component.js';
 
+/**
+ * A Resize Manager. It is in charge of trigger `playerresize` on the player on the right conditions.
+ *
+ * It'll either create an iframe and use a debounced resize handler on it or use the new {@link https://wicg.github.io/ResizeObserver/|ResizeObserver}.
+ *
+ * If the ResizeObserver is available natively, it will be used. A polyfill can be passed in as an option.
+ * If a `playerresize` event is not needed, the ResizeManager component can be removed from the player, see the example below.
+ * @example <caption>How to disable the resize manager</caption>
+ * const player = videojs('#vid', {
+ *   resizeManager: false
+ * });
+ *
+ * @see {@link https://wicg.github.io/ResizeObserver/|ResizeObserver specification}
+ *
+ * @extends Component
+ */
 class ResizeManager extends Component {
+
+  /**
+   * Create the ResizeMAnager.
+   *
+   * @param {Object} player
+   *        The `Player` that this class should be attached to.
+   *
+   * @param {Object} [options]
+   *        The key/value store of ResizeManager options.
+   *
+   * @param {Object} [options.ResizeObserver]
+   *        A polyfill for ResizeObserver can be passed in here.
+   *        If this is set to null it will ignore the native ResizeObserver and fall back to the iframe fallback.
+   */
   constructor(player, options) {
     let RESIZE_OBSERVER_AVAILABLE = options.ResizeObserver || window.ResizeObserver;
 
@@ -49,7 +79,18 @@ class ResizeManager extends Component {
     });
   }
 
+  /**
+   * Called when a resize is triggered on the iframe or a resize is observed via the ResizeObserver
+   *
+   * @fires Player#playerresize
+   */
   resizeHandler() {
+    /**
+     * Called when the player size has changed
+     *
+     * @event Player#playerresize
+     * @type {EventTarget~Event}
+     */
     this.player_.trigger('playerresize');
   }
 
