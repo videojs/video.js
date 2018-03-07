62ff3f66a50c0a908188de9c140011e7c5fd44dc
David GG <hola@davidgg.es>
2018-03-07 20:34:16 +0100

    feat: add mimetype type to source object when possible (#4469) (#4947)
    File mimetype is filled when the file extension is known and type is not provided.
    
    Fixes #4469, improves #4851.

--- src/js/utils/filter-source.js
+++ src/js/utils/filter-source.js
@@ -2,6 +2,8 @@
  * @module filter-source
  */
 import {isObject} from './obj';
+import {MimetypesKind} from './mimetypes';
+import * as Url from '../utils/url.js';
 
 /**
  * Filter out single bad source objects or multiple source objects in an
@@ -34,10 +36,10 @@ const filterSource = function(src) {
     src = newsrc;
   } else if (typeof src === 'string' && src.trim()) {
     // convert string into object
-    src = [{src}];
+    src = [checkMimetype({src})];
   } else if (isObject(src) && typeof src.src === 'string' && src.src && src.src.trim()) {
     // src is already valid
-    src = [src];
+    src = [checkMimetype(src)];
   } else {
     // invalid source, turn it into an empty array
     src = [];
@@ -45,5 +47,24 @@ const filterSource = function(src) {
 
   return src;
 };
+
+/**
+ * Checks src mimetype, adding it when possible
+ *
+ * @param {Tech~SourceObject} src
+ *        The src object to check
+ * @return {Tech~SourceObject}
+ *        src Object with known type
+ */
+function checkMimetype(src) {
+  const ext = Url.getFileExtension(src.src);
+  const mimetype = MimetypesKind[ext.toLowerCase()];
+
+  if (!src.type && mimetype) {
+    src.type = mimetype;
+  }
+
+  return src;
+}
 
 export default filterSource;
---
+++ src/js/utils/mimetypes.js
@@ -0,0 +1,19 @@
+/**
+ * Mimetypes
+ *
+ * @see http://hul.harvard.edu/ois/////systems/wax/wax-public-help/mimetypes.htm
+ * @typedef Mimetypes~Kind
+ * @enum
+ */
+export const MimetypesKind = {
+  opus: 'video/ogg',
+  ogv: 'video/ogg',
+  mp4: 'video/mp4',
+  mov: 'video/mp4',
+  m4v: 'video/mp4',
+  mkv: 'video/x-matroska',
+  mp3: 'audio/mpeg',
+  aac: 'audio/aac',
+  oga: 'audio/ogg',
+  m3u8: 'application/x-mpegURL'
+};
