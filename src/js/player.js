e38ae9419e2bbf0f7d216017e6aeaeca992de222
azanbertre <pedro.hov.borges@gmail.com>
2018-03-10 11:55:06 -0300

    make fix only target desktop chrome

--- src/js/player.js
+++ src/js/player.js
@@ -2636,11 +2636,11 @@ class Player extends Component {
         // only target desktop chrome
         (browser.IS_CHROME && !browser.IS_ANDROID)) {
       this.play();
-    } else {
-      // reload to initial state if autoplay is disabled in desktop chrome
-      if (browser.IS_CHROME && !browser.IS_ANDROID) {
-        this.load();
-      }
+    } else if ((!this.autoplay() || !this.options_.autoplay) &&
+              // only target desktop chrome
+              (browser.IS_CHROME && !browser.IS_ANDROID)) {
+      // reload to initial state if autoplay is disabled
+      this.load();
     }
   }
 
