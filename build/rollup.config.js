ce08f795a85dfde18dc57c0b784eb7661fabc200
Gary Katsevman <git@gkatsev.com>
2018-03-26 16:55:52 -0400

    rollup config

---
+++ build/rollup.config.js
@@ -0,0 +1,30 @@
+import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
+
+export default [
+  {
+    entry: 'src/js/video.js',
+    output: {
+      file: 'src/js/bundle.js',
+      format: 'es'
+    },
+    onwarn(warning) {
+      if (warning.code === 'UNUSED_EXTERNAL_IMPORT' ||
+          warning.code === 'UNRESOLVED_IMPORT') {
+          return;
+        }
+
+      // eslint-disable-next-line no-console
+      console.warn(warning.message || warning);
+    },
+    plugins: [
+      json(),
+      babel({
+        babelrc: false,
+        exclude: 'node_modules/**',
+        presets: [['es2015', { loose: true, modules: false }]],
+        plugins: ['external-helpers']
+      })
+    ]
+  }
+];
