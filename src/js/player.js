6e55f9917a1b3a1623b0be8cd7f6762eccc9c697
Gary Katsevman <git@gkatsev.com>
2018-05-22 14:19:25 -0400

    add missing articles

--- src/js/player.js
+++ src/js/player.js
@@ -589,7 +589,7 @@ class Player extends Component {
     Object.getOwnPropertyNames(attrs).forEach(function(attr) {
       // don't copy over the class attribute to the player element when we're in a div embed
       // the class is already set up properly in the divEmbed case
-      // and we want to make sure that `video-js` class doesn't get lost
+      // and we want to make sure that the `video-js` class doesn't get lost
       if (!(divEmbed && attr === 'class')) {
         el.setAttribute(attr, attrs[attr]);
       }
