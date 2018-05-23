a0c2166f4321663a0bed2803cc02fa0418220fa1
brandonocasey <brandonocasey@gmail.com>
2018-05-23 14:45:52 -0400

    refactor: removed old bug work-around code

--- src/js/player.js
+++ src/js/player.js
@@ -1144,19 +1144,6 @@ class Player extends Component {
 
     // Update the duration if available
     this.handleTechDurationChange_();
-
-    // Chrome and Safari both have issues with autoplay.
-    // In Safari (5.1.1), when we move the video element into the container div, autoplay doesn't work.
-    // In Chrome (15), if you have autoplay + a poster + no controls, the video gets hidden (but audio plays)
-    // This fixes both issues. Need to wait for API, so it updates displays correctly
-    if ((this.src() || this.currentSrc()) && this.tag && this.options_.autoplay && this.paused()) {
-      try {
-        // Chrome Fix. Fixed in Chrome v16.
-        delete this.tag.poster;
-      } catch (e) {
-        log('deleting tag.poster throws in some browsers', e);
-      }
-    }
   }
 
   /**
