8679464529487698aa114caa1aa34e90d4d72681
jforbes <jforbes@brightcove.com>
2018-07-17 16:15:05 -0400

    feat: prefer to use MSE over native for HLS playback

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 import videojs from './video';
 import '@videojs/http-streaming';
+
+// Prefer to use MSE for playback over native
+videojs.options.hls.overrideNative = !videojs.browser.IS_ANY_SAFARI;
+
 export default videojs;
