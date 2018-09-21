2764f9ce82e7f7bab5651e3c9eba72597a8569be
Gary Katsevman <git@gkatsev.com>
2018-09-21 12:19:20 -0400

    get Map from window

--- src/js/event-target.js
+++ src/js/event-target.js
@@ -164,14 +164,14 @@ let EVENT_MAP;
 EventTarget.prototype.queueTrigger = function(event) {
   // only set up EVENT_MAP if it'll be used
   if (!EVENT_MAP) {
-    EVENT_MAP = new Map();
+    EVENT_MAP = new window.Map();
   }
 
   const type = event.type || event;
   let map = EVENT_MAP.get(this);
 
   if (!map) {
-    map = new Map();
+    map = new window.Map();
     EVENT_MAP.set(this, map);
   }
 
