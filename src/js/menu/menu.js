58f638e99daf9e5f4cfb2b6c34c82b84230170b6
Grzegorz Blaszczyk <gjanblaszczyk@gmail.com>
2018-11-02 21:51:42 +0100

    fix: vjs-lock-showing class gets removed from menu when no longer hovering on menu-button. (#5465)
    Fixes #1690

--- src/js/menu/menu.js
+++ src/js/menu/menu.js
@@ -2,6 +2,7 @@
  * @file menu.js
  */
 import Component from '../component.js';
+import document from 'global/document';
 import * as Dom from '../utils/dom.js';
 import * as Fn from '../utils/fn.js';
 import * as Events from '../utils/events.js';
@@ -45,7 +46,8 @@ class Menu extends Component {
    */
   addItem(component) {
     this.addChild(component);
-    component.on('click', Fn.bind(this, function(event) {
+    component.on('blur', Fn.bind(this, this.handleBlur));
+    component.on(['tap', 'click'], Fn.bind(this, function(event) {
       // Unpress the associated MenuButton, and move focus back to it
       if (this.menuButton_) {
         this.menuButton_.unpressButton();
@@ -95,6 +97,29 @@ class Menu extends Component {
     this.contentEl_ = null;
 
     super.dispose();
+  }
+
+  /**
+   * Called when a `MenuItem` loses focus.
+   *
+   * @param {EventTarget~Event} event
+   *        The `blur` event that caused this function to be called.
+   *
+   * @listens blur
+   */
+  handleBlur(event) {
+    const relatedTarget = event.relatedTarget || document.activeElement;
+
+    // Close menu popup when a user clicks outside the menu
+    if (!this.children().some((element) => {
+      return element.el() === relatedTarget;
+    })) {
+      const btn = this.menuButton_;
+
+      if (btn && btn.buttonPressed_ && relatedTarget !== btn.el().firstChild) {
+        btn.unpressButton();
+      }
+    }
   }
 
   /**
