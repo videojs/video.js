78b3dd7682c731f716b68e5ca9f5a0b401e56e06
Gary Katsevman <git@gkatsev.com>
2018-11-30 14:10:41 -0500

    sandbox script

--- build/sandbox.js
+++ build/sandbox.js
@@ -1,5 +1,28 @@
-#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const klawSync = require('klaw-sync');
 
-const generateExample = require('./generate-example.js').default;
+const files = klawSync('sandbox/').filter((file) => path.extname(file.path) === '.example');
 
-generateExample({skipBuild: true});
+const changes = files.map(function(file) {
+  const p = path.parse(file.path);
+  const nonExample = path.join(p.dir, p.name);
+  return {
+    file: file.path,
+    copy: nonExample
+  };
+})
+.filter(function(change) {
+  return !fs.existsSync(change.copy);
+});
+
+changes.forEach(function(change) {
+  fs.copyFileSync(change.file, change.copy);
+});
+
+if (changes.length) {
+  console.log("Updated Sandbox files for:");
+  console.log('\t' + changes.map((chg) => chg.copy).join('\n\t'));
+} else {
+  console.log("No sandbox updates necessary");
+}
