41291430858aa3a75de1b0251145981c47b0b66f
Liu Ruen-shen <kiwi71728@icloud.com>
2019-01-23 11:42:37 +0800

    fix: return boolean value in array.filter callback

--- src/js/menu/menu.js
+++ src/js/menu/menu.js
@@ -188,11 +188,7 @@ class Menu extends Component {
         return;
       }
 
-      const foundComponent = childComponents.filter(component => {
-        if (component.el() === event.target) {
-          return component;
-        }
-      })[0];
+      const foundComponent = childComponents.filter(component => component.el() === event.target)[0];
 
       if (!foundComponent) {
         return;
