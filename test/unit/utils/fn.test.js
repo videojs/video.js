4295ff217a3c281d6f8b6ea60f076b97e4bc43f8
Thijs Triemstra <info@collab.nl>
2019-04-23 23:20:00 +0200

    only once

--- test/unit/utils/fn.test.js
+++ test/unit/utils/fn.test.js
@@ -6,9 +6,6 @@ import * as Fn from '../../../src/js/utils/fn.js';
 QUnit.module('fn', {
   beforeEach() {
     this.clock = sinon.useFakeTimers();
-    // stub performance.now for sinon, see:
-    // https://github.com/sinonjs/sinon/issues/803
-    sinon.stub(window.performance, 'now', Date.now);
   },
   afterEach() {
     this.clock.restore();
@@ -26,6 +23,10 @@ QUnit.test('should add context to a function', function(assert) {
 });
 
 QUnit.test('should throttle functions properly', function(assert) {
+  // stub performance.now for sinon, see:
+  // https://github.com/sinonjs/sinon/issues/803
+  sinon.stub(window.performance, 'now', Date.now);
+
   const tester = sinon.spy();
   const throttled = Fn.throttle(tester, 100);
 
