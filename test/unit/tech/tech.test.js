815d78fa94dfced7b0007d46ca5b6cb16bcab114
brandonocasey <brandonocasey@gmail.com>
2019-04-26 15:29:11 -0400

    more test cleanup

--- test/unit/tech/tech.test.js
+++ test/unit/tech/tech.test.js
@@ -338,11 +338,12 @@ QUnit.test('should add the source handler interface to a tech', function(assert)
   // and provde a dispose method for the handler.
   // This is optional for source handlers
   let disposeCalled = false;
-  const HandlerInternalState = function() {};
 
-  HandlerInternalState.prototype.dispose = function() {
-    disposeCalled = true;
-  };
+  class HandlerInternalState {
+    dispose() {
+      disposeCalled = true;
+    }
+  }
 
   // Create source handlers
   const handlerOne = {
