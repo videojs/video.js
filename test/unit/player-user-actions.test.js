e947c20a0182478b05233aa6f3335f64801c504d
Pat O'Neill <pgoneill@gmail.com>
2019-05-06 16:19:57 -0400

    Add element include/exclude logic tests

--- test/unit/player-user-actions.test.js
+++ test/unit/player-user-actions.test.js
@@ -1,24 +1,8 @@
 /* eslint-env qunit */
+import document from 'global/document';
 import keycode from 'keycode';
 import sinon from 'sinon';
 import TestHelpers from './test-helpers';
-
-QUnit.module('Player: User Actions', {
-
-  beforeEach() {
-    this.clock = sinon.useFakeTimers();
-    this.player = TestHelpers.makePlayer({controls: true});
-  },
-
-  afterEach() {
-    this.player.dispose();
-    this.clock.restore();
-  }
-});
-
-QUnit.test('userActions are disabled by default', function(assert) {
-  assert.strictEqual(this.player.options_.userActions, undefined, 'userActions are disabled by default');
-});
 
 QUnit.module('Player: User Actions: Double Click', {
 
@@ -158,7 +142,7 @@ const mockKeyDownEvent = (key) => {
   };
 };
 
-const executeKeyTest = {
+const defaultKeyTests = {
   fullscreen(player, assert, positive) {
     let fullscreen;
 
@@ -271,12 +255,13 @@ const executeKeyTest = {
 
 QUnit.test('by default, hotkeys are disabled', function(assert) {
   assert.expect(14);
-  executeKeyTest.fullscreen(this.player, assert, false);
-  executeKeyTest.mute(this.player, assert, false);
-  executeKeyTest.playPause(this.player, assert, false);
+  defaultKeyTests.fullscreen(this.player, assert, false);
+  defaultKeyTests.mute(this.player, assert, false);
+  defaultKeyTests.playPause(this.player, assert, false);
 });
 
 QUnit.test('when userActions.hotkeys is true, hotkeys are enabled', function(assert) {
+  this.player.dispose();
   this.player = TestHelpers.makePlayer({
     controls: true,
     userActions: {
@@ -285,12 +270,13 @@ QUnit.test('when userActions.hotkeys is true, hotkeys are enabled', function(ass
   });
 
   assert.expect(16);
-  executeKeyTest.fullscreen(this.player, assert, true);
-  executeKeyTest.mute(this.player, assert, true);
-  executeKeyTest.playPause(this.player, assert, true);
+  defaultKeyTests.fullscreen(this.player, assert, true);
+  defaultKeyTests.mute(this.player, assert, true);
+  defaultKeyTests.playPause(this.player, assert, true);
 });
 
 QUnit.test('when userActions.hotkeys is an object, hotkeys are enabled', function(assert) {
+  this.player.dispose();
   this.player = TestHelpers.makePlayer({
     controls: true,
     userActions: {
@@ -299,12 +285,13 @@ QUnit.test('when userActions.hotkeys is an object, hotkeys are enabled', functio
   });
 
   assert.expect(16);
-  executeKeyTest.fullscreen(this.player, assert, true);
-  executeKeyTest.mute(this.player, assert, true);
-  executeKeyTest.playPause(this.player, assert, true);
+  defaultKeyTests.fullscreen(this.player, assert, true);
+  defaultKeyTests.mute(this.player, assert, true);
+  defaultKeyTests.playPause(this.player, assert, true);
 });
 
 QUnit.test('when userActions.hotkeys.fullscreenKey can be a function', function(assert) {
+  this.player.dispose();
   this.player = TestHelpers.makePlayer({
     controls: true,
     userActions: {
@@ -339,6 +326,7 @@ QUnit.test('when userActions.hotkeys.fullscreenKey can be a function', function(
 });
 
 QUnit.test('when userActions.hotkeys.muteKey can be a function', function(assert) {
+  this.player.dispose();
   this.player = TestHelpers.makePlayer({
     controls: true,
     userActions: {
@@ -373,6 +361,7 @@ QUnit.test('when userActions.hotkeys.muteKey can be a function', function(assert
 });
 
 QUnit.test('when userActions.hotkeys.playPauseKey can be a function', function(assert) {
+  this.player.dispose();
   this.player = TestHelpers.makePlayer({
     controls: true,
     userActions: {
@@ -405,4 +394,86 @@ QUnit.test('when userActions.hotkeys.playPauseKey can be a function', function(a
 
   assert.strictEqual(this.player.pause.callCount, 1, 'has paused');
   assert.strictEqual(this.player.play.callCount, 1, 'has played');
+});
+
+QUnit.test('hotkeys are ignored when focus is in a textarea', function(assert) {
+  this.player.dispose();
+  this.player = TestHelpers.makePlayer({
+    controls: true,
+    userActions: {
+      hotkeys: true
+    }
+  });
+
+  const textarea = document.createElement('textarea');
+
+  this.player.el_.appendChild(textarea);
+  textarea.focus();
+
+  assert.expect(14);
+  defaultKeyTests.fullscreen(this.player, assert, false);
+  defaultKeyTests.mute(this.player, assert, false);
+  defaultKeyTests.playPause(this.player, assert, false);
+});
+
+QUnit.test('hotkeys are ignored when focus is in a text input', function(assert) {
+  this.player.dispose();
+  this.player = TestHelpers.makePlayer({
+    controls: true,
+    userActions: {
+      hotkeys: true
+    }
+  });
+
+  const input = document.createElement('input');
+
+  input.type = 'text';
+  this.player.el_.appendChild(input);
+  input.focus();
+
+  assert.expect(14);
+  defaultKeyTests.fullscreen(this.player, assert, false);
+  defaultKeyTests.mute(this.player, assert, false);
+  defaultKeyTests.playPause(this.player, assert, false);
+});
+
+QUnit.test('hotkeys are NOT ignored when focus is on a button element', function(assert) {
+  this.player.dispose();
+  this.player = TestHelpers.makePlayer({
+    controls: true,
+    userActions: {
+      hotkeys: true
+    }
+  });
+
+  const button = document.createElement('button');
+
+  this.player.el_.appendChild(button);
+  button.focus();
+
+  assert.expect(16);
+  defaultKeyTests.fullscreen(this.player, assert, true);
+  defaultKeyTests.mute(this.player, assert, true);
+  defaultKeyTests.playPause(this.player, assert, true);
+});
+
+QUnit.test('hotkeys are NOT ignored when focus is on a button input', function(assert) {
+  this.player.dispose();
+  this.player = TestHelpers.makePlayer({
+    controls: true,
+    userActions: {
+      hotkeys: true
+    }
+  });
+
+  const input = document.createElement('input');
+
+  input.type = 'button';
+  this.player.el_.appendChild(input);
+  input.focus();
+
+  assert.expect(16);
+  defaultKeyTests.fullscreen(this.player, assert, true);
+  defaultKeyTests.mute(this.player, assert, true);
+  defaultKeyTests.playPause(this.player, assert, true);
 });
