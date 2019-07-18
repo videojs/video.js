32cd08fadee3818506f207af74f785b62de0ad1d
brandonocasey <brandonocasey@gmail.com>
2019-07-18 15:02:46 -0400

    bring the comment back

--- src/js/utils/dom-data.js
+++ src/js/utils/dom-data.js
@@ -3,4 +3,14 @@
  * @module dom-data
  */
 
+/**
+ * Element Data Store.
+ *
+ * Allows for binding data to an element without putting it directly on the
+ * element. Ex. Event listeners are stored here.
+ * (also from jsninja.com, slightly modified and updated for closure compiler)
+ *
+ * @type {Object}
+ * @private
+ */
 export default new WeakMap();
