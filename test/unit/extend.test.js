25d15d4b1487e84b493cb055cfa30533fce98ef9
Gary Katsevman <git@gkatsev.com>
2019-11-22 12:32:18 -0500

    fix(extend): super_ should be available for backwards compatibility (#6329)
    Fixes #6328

--- test/unit/extend.test.js
+++ test/unit/extend.test.js
@@ -16,3 +16,15 @@ QUnit.test('should add implicit parent constructor call', function(assert) {
   assert.ok(superCalled, 'super constructor called');
   assert.ok(child.foo, 'child properties set');
 });
+
+QUnit.test('should have a super_ pointer', function(assert) {
+  const Parent = function() {};
+  const Child = extend(Parent, {
+    foo: 'bar'
+  });
+
+  const child = new Child();
+
+  assert.ok(child.foo, 'child properties set');
+  assert.equal(child.constructor.super_, Parent, 'super_ is present and equal to the super class');
+});
