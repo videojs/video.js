d9759813ee63797bc6028587ff52c1305fa5ffaa
Gary Katsevman <git@gkatsev.com>
2020-04-22 01:07:14 -0400

    unskip html5 tech test

--- test/unit/tech/html5.test.js
+++ test/unit/tech/html5.test.js
@@ -51,7 +51,7 @@ QUnit.module('HTML5', {
   }
 });
 
-QUnit.skip('if setScrubbing is true and fastSeek is available, use it', function(assert) {
+QUnit.test('if setScrubbing is true and fastSeek is available, use it', function(assert) {
   Object.defineProperty(tech.el(), 'currentTime', {
     get: () => {},
     set: () => {},
