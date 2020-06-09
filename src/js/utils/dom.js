0632f429feac621bc9dba2f21350b9dd15b51e42
TravisBader <travis.Bader@faithlife.com>
2020-06-09 14:57:26 -0700

    fix: check element for null in case of a disposed player

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -280,6 +280,10 @@ export function addClass(element, classToAdd) {
  *         The DOM element with class name removed.
  */
 export function removeClass(element, classToRemove) {
+  // Protect in case the player gets disposed
+  if (!element) {
+    return null;
+  }
   if (element.classList) {
     element.classList.remove(classToRemove);
   } else {
