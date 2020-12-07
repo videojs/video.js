6840569f448b673e4efbb81aa0514263bdf8600b
brandonocasey <brandonocasey@gmail.com>
2020-12-07 15:15:48 -0500

    fix: prevent dispose error and text track duplicate listeners

--- src/js/tracks/text-track.js
+++ src/js/tracks/text-track.js
@@ -184,7 +184,10 @@ class TextTrack extends Track {
     const activeCues = new TextTrackCueList(this.activeCues_);
     let changed = false;
     const timeupdateHandler = Fn.bind(this, function() {
+      if (!this.tech_.isReady_ || this.tech_.isDisposed()) {
+        return;
 
+      }
       // Accessing this.activeCues for the side-effects of updating itself
       // due to its nature as a getter function. Do not remove or cues will
       // stop updating!
@@ -196,10 +199,14 @@ class TextTrack extends Track {
       }
     });
 
+    const disposeHandler = () => {
+      this.tech_.off('timeupdate', timeupdateHandler);
+      this.tech_.off('dispose', disposeHandler);
+    };
+
+    this.tech_.on('dispose', disposeHandler);
     if (mode !== 'disabled') {
-      this.tech_.ready(() => {
-        this.tech_.on('timeupdate', timeupdateHandler);
-      }, true);
+      this.tech_.on('timeupdate', timeupdateHandler);
     }
 
     Object.defineProperties(this, {
@@ -241,12 +248,10 @@ class TextTrack extends Track {
             // On-demand load.
             loadTrack(this.src, this);
           }
+          this.tech_.off('timeupdate', timeupdateHandler);
+
           if (mode !== 'disabled') {
-            this.tech_.ready(() => {
-              this.tech_.on('timeupdate', timeupdateHandler);
-            }, true);
-          } else {
-            this.tech_.off('timeupdate', timeupdateHandler);
+            this.tech_.on('timeupdate', timeupdateHandler);
           }
           /**
            * An event that fires when mode changes on this track. This allows
