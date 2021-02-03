47a23401a71a358aa8f731ba32438e2579b4e4d3
Gary Katsevman <git@gkatsev.com>
2021-02-03 17:18:06 -0500

    remove old findPosition

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -581,40 +581,6 @@ export function findPosition(el) {
   };
 }
 
-export function findPosition2(el) {
-  let box;
-
-  if (el.getBoundingClientRect && el.parentNode) {
-    box = el.getBoundingClientRect();
-  }
-
-  if (!box) {
-    return {
-      left: 0,
-      top: 0
-    };
-  }
-
-  const docEl = document.documentElement;
-  const body = document.body;
-
-  const clientLeft = docEl.clientLeft || body.clientLeft || 0;
-  const scrollLeft = window.pageXOffset || body.scrollLeft;
-  const left = box.left + scrollLeft - clientLeft;
-
-  const clientTop = docEl.clientTop || body.clientTop || 0;
-  const scrollTop = window.pageYOffset || body.scrollTop;
-  const top = box.top + scrollTop - clientTop;
-
-  // Android sometimes returns slightly off decimal values, so need to round
-  return {
-    left: Math.round(left),
-    top: Math.round(top),
-    width: box.width,
-    height: box.height
-  };
-}
-
 /**
  * Represents x and y coordinates for a DOM element or mouse pointer.
  *
