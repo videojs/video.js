c25f6aea62520b12873f77421a7ec2e8146cf8c7
Gary Katsevman <git@gkatsev.com>
2021-07-02 15:25:58 -0400

    add Safari 9

--- test/sinon.js
+++ test/sinon.js
@@ -2,8 +2,8 @@
 
 const s = document.createElement('script');
 
-// on IE11, load the last supported sinon version
-if (/(?:MSIE|Trident\/7.0)/.test(navigator.userAgent)) {
+// on IE11 and Safari 9, load the last supported sinon version
+if (/(?:MSIE|Trident\/7.0|Version\/9.*Safari)/.test(navigator.userAgent)) {
   s.src = 'https://unpkg.com/sinon@9.2.4/pkg/sinon-no-sourcemaps.js';
 } else {
   s.src = '/test/base/node_modules/sinon/pkg/sinon.js';
