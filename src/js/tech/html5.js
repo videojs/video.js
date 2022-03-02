1179826cbc316fb264c092c30f86eb5ab01cab5b
mister-ben <git@misterben.me>
2022-03-02 16:34:13 +0100

    feat: Greater text track precision using requestVideoFrameCallback (#7633)

--- src/js/tech/html5.js
+++ src/js/tech/html5.js
@@ -742,6 +742,32 @@ class Html5 extends Tech {
   }
 
   /**
+   * Native requestVideoFrameCallback if supported by browser/tech, or fallback
+   *
+   * @param {function} cb function to call
+   * @return {number} id of request
+   */
+  requestVideoFrameCallback(cb) {
+    if (this.featuresVideoFrameCallback) {
+      return this.el_.requestVideoFrameCallback(cb);
+    }
+    return super.requestVideoFrameCallback(cb);
+  }
+
+  /**
+   * Native or fallback requestVideoFrameCallback
+   *
+   * @param {number} id request id to cancel
+   */
+  cancelVideoFrameCallback(id) {
+    if (this.featuresVideoFrameCallback) {
+      this.el_.cancelVideoFrameCallback(id);
+    } else {
+      super.cancelVideoFrameCallback(id);
+    }
+  }
+
+  /**
    * A getter/setter for the `Html5` Tech's source object.
    * > Note: Please use {@link Html5#setSource}
    *
@@ -1298,6 +1324,13 @@ Html5.prototype.featuresProgressEvents = true;
  * @default
  */
 Html5.prototype.featuresTimeupdateEvents = true;
+
+/**
+ * Whether the HTML5 el supports `requestVideoFrameCallback`
+ *
+ * @type {boolean}
+ */
+Html5.prototype.featuresVideoFrameCallback = !!(Html5.TEST_VID && Html5.TEST_VID.requestVideoFrameCallback);
 
 // HTML5 Feature detection and Device Fixes --------------------------------- //
 let canPlayType;
