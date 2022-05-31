a14ace202b564a9f8bc30f54a447b13d99343fb4
mister-ben <git@misterben.me>
2022-05-31 21:50:51 +0200

    fix: HTML5 tech with audio tag shouldn't use requestVideoFrameCallback (#7778)

--- test/unit/tech/html5.test.js
+++ test/unit/tech/html5.test.js
@@ -1032,3 +1032,12 @@ QUnit.test('supports getting available media playback quality metrics', function
   window.performance = origPerformance;
   window.Date = origDate;
 });
+
+QUnit.test('featuresVideoFrameCallback is false for audio elements', function(assert) {
+  const el = document.createElement('audio');
+  const audioTech = new Html5({el});
+
+  assert.strictEqual(audioTech.featuresVideoFrameCallback, false, 'Html5 with audio element should not support rvf');
+
+  audioTech.dispose();
+});
