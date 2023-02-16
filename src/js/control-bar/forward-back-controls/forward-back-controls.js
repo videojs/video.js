2614c36a3a6e9c712c7eb8f49db68c5b72eece92
usmanonazim <usmanonazim@gmail.com>
2023-02-16 16:44:17 +0000

    add jsdoc comments for clarity

--- src/js/control-bar/forward-back-controls/forward-back-controls.js
+++ src/js/control-bar/forward-back-controls/forward-back-controls.js
@@ -1,7 +1,13 @@
 import Component from '../../component';
-import * as Dom from '../../utils/dom';
 import Button from '../../button';
 
+/**
+ * A component that contains both a 'Forward' and a 'Back' button.
+ * These buttons allow the user to jump forward or back in the timeline
+ * of a video by a certain number of seconds (either 5, 10, or 30)
+ *
+ * @extends Component
+ */
 class ForwardBackControls extends Component {
   constructor(player, options) {
     super(player, options);
@@ -14,26 +20,44 @@ class ForwardBackControls extends Component {
 
     this.populateForwardAndBackButtons();
 
+    // should only show this component if user has set the `jumpForwardAndBack` option to a valid time
     if (this.jumpForwardAndBackTime && this.validOptions.includes(this.jumpForwardAndBackTime)) {
       this.show();
     } else {
       this.hide();
     }
   }
-
+  /**
+   * Populates the forward and back buttons with the relevant
+   * class names, control text and onClick functionality.
+   */
   populateForwardAndBackButtons() {
     this.forwardButton_.addClass(`vjs-forward-button-${this.jumpForwardAndBackTime}`);
     this.forwardButton_.controlText('Jump Forward');
+
     this.backButton_.addClass(`vjs-back-button-${this.jumpForwardAndBackTime}`);
     this.backButton_.controlText('Jump Back');
 
     this.on(this.forwardButton_, 'click', (e) => this.handleForwardClick(e));
+    this.on(this.forwardButton_, 'tap', (e) => this.handleForwardClick(e));
+
     this.on(this.backButton_, 'click', (e) => this.handleBackClick(e));
+    this.on(this.backButton_, 'tap', (e) => this.handleBackClick(e));
 
     this.addChild(this.forwardButton_);
     this.addChild(this.backButton_);
   }
 
+  /**
+   * Handle a click on a `Forward Button`
+   * Moves the current time on a video forward by x seconds,
+   * or if the amount of time remaining is less than x,
+   * moves the video to the end.
+   *
+   * @param {EventTarget~Event} event
+   *        The `tap` or `click` event that caused this function
+   *        to be called
+   */
   handleForwardClick(event) {
     const currentTime = this.player_.currentTime();
     const duration = this.player_.duration();
@@ -48,13 +72,23 @@ class ForwardBackControls extends Component {
     this.player_.currentTime(newTime);
   }
 
+  /**
+   * Handle a click on a `Back Button`
+   * Moves the current time on a video back by x seconds,
+   * or if the current time is less than x, moves the
+   * video to the beginning
+   *
+   * @param {EventTarget~Event} event
+   *        The `tap` or `click` event that cuased this function
+   *        to be called
+   */
   handleBackClick(event) {
-    const currentVideoTime = this.player_.currentTime();
+    const currentTime = this.player_.currentTime();
 
     let newTime;
 
-    if (currentVideoTime >= this.jumpForwardAndBackTime) {
-      newTime = currentVideoTime - this.jumpForwardAndBackTime;
+    if (currentTime >= this.jumpForwardAndBackTime) {
+      newTime = currentTime - this.jumpForwardAndBackTime;
     } else {
       newTime = 0;
     }
@@ -63,15 +97,6 @@ class ForwardBackControls extends Component {
 
   buildCSSClass() {
     return `vjs-forward-back-controls ${super.buildCSSClass()}`;
-  }
-
-  // we want to create two elements
-  createEl() {
-    const el = Dom.createEl('div', {
-      className: this.buildCSSClass()
-    });
-
-    return el;
   }
 }
 
