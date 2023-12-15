b28bf97a5e6ef2fd9e78f49d6a50f4a6dc7b4a3e
amtins <andre.martins@rts.ch>
2023-12-15 12:36:25 +0100

    refactor(time-tooltip): remove unnecessary code to be able to start from scratch

--- src/js/control-bar/progress-control/time-tooltip.js
+++ src/js/control-bar/progress-control/time-tooltip.js
@@ -52,56 +52,6 @@ class TimeTooltip extends Component {
    *        from the left edge of the {@link SeekBar}
    */
   update(seekBarRect, seekBarPoint, content) {
-    const tooltipRect = Dom.findPosition(this.el_);
-    const playerRect = Dom.getBoundingClientRect(this.player_.el());
-    const seekBarPointPx = seekBarRect.width * seekBarPoint;
-
-    // do nothing if either rect isn't available
-    // for example, if the player isn't in the DOM for testing
-    if (!playerRect || !tooltipRect) {
-      return;
-    }
-
-    // This is the space left of the `seekBarPoint` available within the bounds
-    // of the player. We calculate any gap between the left edge of the player
-    // and the left edge of the `SeekBar` and add the number of pixels in the
-    // `SeekBar` before hitting the `seekBarPoint`
-    const spaceLeftOfPoint = (seekBarRect.left - playerRect.left) + seekBarPointPx;
-
-    // This is the space right of the `seekBarPoint` available within the bounds
-    // of the player. We calculate the number of pixels from the `seekBarPoint`
-    // to the right edge of the `SeekBar` and add to that any gap between the
-    // right edge of the `SeekBar` and the player.
-    const spaceRightOfPoint = (seekBarRect.width - seekBarPointPx) +
-      (playerRect.right - seekBarRect.right);
-
-    // This is the number of pixels by which the tooltip will need to be pulled
-    // further to the right to center it over the `seekBarPoint`.
-    let pullTooltipBy = tooltipRect.width / 2;
-
-    // Adjust the `pullTooltipBy` distance to the left or right depending on
-    // the results of the space calculations above.
-    if (spaceLeftOfPoint < pullTooltipBy) {
-      pullTooltipBy += pullTooltipBy - spaceLeftOfPoint;
-    } else if (spaceRightOfPoint < pullTooltipBy) {
-      pullTooltipBy = spaceRightOfPoint;
-    }
-
-    // Due to the imprecision of decimal/ratio based calculations and varying
-    // rounding behaviors, there are cases where the spacing adjustment is off
-    // by a pixel or two. This adds insurance to these calculations.
-    if (pullTooltipBy < 0) {
-      pullTooltipBy = 0;
-    } else if (pullTooltipBy > tooltipRect.width) {
-      pullTooltipBy = tooltipRect.width;
-    }
-
-    // prevent small width fluctuations within 0.4px from
-    // changing the value below.
-    // This really helps for live to prevent the play
-    // progress time tooltip from jittering
-    pullTooltipBy = Math.round(pullTooltipBy);
-
     this.write(content);
   }
 
