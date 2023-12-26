537f8a10f7e5aaede7ca1069325b9b616a7f8de0
hswaminathan <hswaminathan@brightcove.com>
2023-12-26 15:53:20 -0500

    fix test

--- src/js/modal-dialog.js
+++ src/js/modal-dialog.js
@@ -156,10 +156,8 @@ class ModalDialog extends Component {
    * @fires ModalDialog#modalopen
    */
   open() {
-    if (this.opened_) {
-      if (this.options_.fillAlways) {
-        this.fill();
-      }
+    if (this.opened_ && this.options_.fillAlways) {
+      this.fill();
       return;
     }
 
