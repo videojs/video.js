b8388ef8b739bc62b3dd233ad447e06b331e5696
Carlos Villasenor Castillo <cvillasenor@Carloss-MacBook-Pro.local>
2024-03-20 15:58:11 -0600

    feat(player): Remove unrequired files from 'react-video-nav-app'

--- docs/examples/react-video-nav-app/src/index.js
+++ docs/examples/react-video-nav-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import videojs from '../node_modules/video.js/dist/video.js';
 import '../node_modules/video.js/dist/video-js.css';
 
@@ -13,10 +12,6 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 var vid = document.getElementById('vid1');
 
