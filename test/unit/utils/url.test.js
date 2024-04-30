7fae95699fb465b61b5f731b346a9ae5bf112d48
mister-ben <git@misterben.me>
2024-04-30 10:53:31 +0200

    add relative test

--- test/unit/utils/url.test.js
+++ test/unit/utils/url.test.js
@@ -41,8 +41,7 @@ QUnit.test('should strip port from hosts using http or https', function(assert) 
 });
 
 QUnit.test('should get an absolute URL', function(assert) {
-  // Errors on compiled tests that don't use unit.html. Need a better solution.
-  // assert.ok(Url.getAbsoluteURL('unit.html') === window.location.href);
+  assert.ok(Url.getAbsoluteURL(window.location.pathname + window.location.search) === window.location.href);
   assert.ok(Url.getAbsoluteURL('http://asdf.com') === 'http://asdf.com/');
   assert.ok(Url.getAbsoluteURL('https://asdf.com/index.html') === 'https://asdf.com/index.html');
 });
