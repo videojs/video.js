2b6f69ef2305c7ae6b743016521c1c4bb86f6434
Adam Waldron <awaldron@brightcove.com>
2024-05-03 01:41:33 -0700

    feat: refactor error consts

--- src/js/consts/errors.js
+++ src/js/consts/errors.js
@@ -1,12 +1,16 @@
 export default {
-  UnsupportedSidxContainer: 'unsupported-sidx-container-error',
-  DashManifestSidxParsingError: 'dash-manifest-sidx-parsing-error',
-  HlsPlaylistRequestError: 'hls-playlist-request-error',
-  SegmentUnsupportedMediaFormat: 'segment-unsupported-media-format-error',
-  UnsupportedMediaInitialization: 'unsupported-media-initialization-error',
-  SegmentSwitchError: 'segment-switch-error',
-  SegmentExceedsSourceBufferQuota: 'segment-exceeds-source-buffer-quota-error',
-  SegmentAppendError: 'segment-append-error',
-  VttLoadError: 'vtt-load-error',
-  VttCueParsingError: 'vtt-cue-parsing-error'
+  NetworkBadStatus: 'networkbadstatus',
+  NetworkRequestFailed: 'networkrequestfailed',
+  NetworkRequestAborted: 'networkrequestaborted',
+  NetworkRequestTimeout: 'networkrequesttimeout',
+  NetworkBodyParser: 'networkbodyparser',
+  StreamingHlsPlaylistParserError: 'streaminghlsplaylistparsererror',
+  StreamingDashManifestParserError: 'streamingdashmanifestparsererror',
+  StreamingContentSteeringParserError: 'streamingcontentsteeringparsererror',
+  StreamingVttParserError: 'streamingvttparsererror',
+  StreamingFailedToSelectNextSegment: 'streamingfailedtoselectnextsegment',
+  StreamingFailedToDecryptSegment: 'streamingfailedtodecryptsegment',
+  StreamingFailedToTransmuxSegment: 'streamingfailedtotransmuxsegment',
+  StreamingFailedToAppendSegment: 'streamingfailedtoappendsegment',
+  StreamingCodecsChangeError: 'streamingcodecschangeerror'
 };
