7c2a64e22dc4a3464d503f480066283085389d5b
cvillasenor <carlosdeveloper9@gmail.com>
2024-08-14 12:51:34 -0600

    fix: update condition for update textTrackDisplay styles

--- src/js/tracks/text-track-display.js
+++ src/js/tracks/text-track-display.js
@@ -320,7 +320,7 @@ class TextTrackDisplay extends Component {
       this.updateForTrack(descriptionsTrack);
     }
 
-    if (window.CSS.supports('inset', '10px')) {
+    if (!window.CSS.supports('inset', '10px')) {
       const textTrackDisplay = this.el_;
       const vjsTextTrackCues = textTrackDisplay.querySelectorAll('.vjs-text-track-cue');
 
