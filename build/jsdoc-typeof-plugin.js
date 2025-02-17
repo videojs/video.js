2798afd4924e0978d00b51242892a4560365aab1
mister-ben <1676039+mister-ben@users.noreply.github.com>
2025-02-17 10:16:56 +0100

    support @template {typeof Component} T

--- build/jsdoc-typeof-plugin.js
+++ build/jsdoc-typeof-plugin.js
@@ -1,18 +1,19 @@
 /**
- * The jsdoc plugin that will convert types like {typeof ClassName} into {Class<ClassName>}
+ * The jsdoc plugin to transform some typescript jsdoc to actual jsdoc, so doc generation works.
  */
 exports.handlers = {
   jsdocCommentFound: event => {
-    const templateMatch = /@template {(.*)} ([A-Z])\n/.exec(event.comment || '');
+    // @template {ClassName} T ; {T} -> {typeof ClassName}
+    const templateMatch = /@template {(typeof )?(.*)} ([A-Z])\n/.exec(event.comment || '');
 
     if (templateMatch) {
-      const className = templateMatch[1];
-      const templateName = templateMatch[2];
+      const className = templateMatch[2];
+      const templateName = templateMatch[3];
 
       event.comment = event.comment.replace(new RegExp(`{(.*\\b)(${templateName})(\\b.*)}`, 'g'), `{$1typeof ${className}$3}`);
     }
 
-    // \{.*\bT\b.*\}
+    // {typeof ClassName} -> {Class<ClassName>}
     event.comment = (event.comment || '').replace(/\{.*typeof\s+([^\s\|]+).*\}/g, typeExpression => {
       return typeExpression.replace(/typeof\s+([^\s\|\}]+)/g, (expression, className) => {
         return 'Class<' + className + '>';
