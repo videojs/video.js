836d8cb16e24df600612578574fb93da5c602c95
Christian Pillsbury <cpillsbury@mux.com>
2025-11-14 06:45:50 -0800

    fix: minor change to test trusted publishing workflow e2e

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,5 @@ import videojs from './video';
 import 'videojs-contrib-quality-levels';
 import '@videojs/http-streaming';
 export default videojs;
+// eslint-disable-next-line no-console
+console.log('THIS IS A VALIDATION OF RELEASES!');
