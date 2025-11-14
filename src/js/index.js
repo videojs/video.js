f3de5f35c91dcec9de6ed304c8d99c31f073ed61
Christian Pillsbury <cpillsbury@mux.com>
2025-11-14 07:10:18 -0800

    fix: revert minor change to test trusted publishing workflow e2e sans npm token in release.yml

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,3 @@ import videojs from './video';
 import 'videojs-contrib-quality-levels';
 import '@videojs/http-streaming';
 export default videojs;
-// eslint-disable-next-line no-console
-console.log('THIS IS A VALIDATION OF RELEASES!');
