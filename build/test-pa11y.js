82abebaee04272466c548eeaf79b5f219749e057
Daniel Rossi <electroteque@protonmail.com>
2026-04-04 08:57:36 +1100

    fix linting

--- build/test-pa11y.js
+++ build/test-pa11y.js
@@ -3,16 +3,15 @@ const path = require('path');
 
 const testFile = path.join(__dirname, '..', 'sandbox', 'descriptions.test-a11y.html');
 const config = {
-    ignore: [
-        // Ignore warning about contrast of the "vjs-no-js" fallback link
-        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.BgImage'
-    ],
-    includeWarnings: true,
-    "levelCapWhenNeedsReview": "warning",
-    standard: 'WCAG2AA'
+  ignore: [
+    // Ignore warning about contrast of the "vjs-no-js" fallback link
+    'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.BgImage'
+  ],
+  includeWarnings: true,
+  levelCapWhenNeedsReview: 'warning',
+  standard: 'WCAG2AA'
 };
 
-pa11y(testFile,config).then((results) => {
-    // Use the results
-    console.log(results);
+pa11y(testFile, config).then((results) => {
+   process.stdout.write(JSON.stringify(results, null, 2));
 });
