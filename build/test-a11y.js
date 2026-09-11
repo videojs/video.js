29bc1b36f7f8f6232c820b19a7f3c67d6c4ebb21
Santiago Puppo <spuppo@mux.com>
2026-09-11 14:14:04 -0300

    fix: replaced access-sniff with pa11y, updated test-a11y.

--- build/test-a11y.js
+++ build/test-a11y.js
@@ -1,4 +1,5 @@
-const AccessSniff = require('access-sniff');
+/* eslint-disable no-console */
+const pa11y = require('pa11y');
 const path = require('path');
 
 const testFiles = [
@@ -6,22 +7,43 @@ const testFiles = [
 ];
 
 const options = {
-  accessibilityLevel: 'WCAG2AA',
-  reportLevels: {
-    notice: false,
-    warning: true,
-    error: true
-  },
+  standard: 'WCAG2AA',
+  includeWarnings: true,
+  includeNotices: false,
   ignore: [
     // Ignore warning about contrast of the "vjs-no-js" fallback link
     'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.BgImage'
   ]
 };
 
-AccessSniff.default(testFiles, options).then(function(report) {
-  AccessSniff.report(report);
-}).catch(function() {
+Promise.all(testFiles.map((file) => pa11y(`file://${file}`, options))).
+  then((results) => {
+    let errorCount = 0;
 
-  // there were errors, which are already reported, exit with an error
-  process.exit(1);
-});
+    results.forEach((result) => {
+      console.log(`\nTesting ${result.pageUrl}\n`);
+
+      if (!result.issues.length) {
+        console.log('  no issues found');
+        return;
+      }
+
+      result.issues.forEach((issue) => {
+        errorCount += issue.type === 'error' ? 1 : 0;
+        console.log(`  [${issue.type}] ${issue.code}`);
+        console.log(`    ${issue.message}`);
+        console.log(`    ${issue.selector}`);
+      });
+    });
+
+    if (errorCount) {
+      console.log(`\n${errorCount} accessibility errors\n`);
+      process.exit(1);
+    }
+
+    console.log('\nAccessibility tests passed\n');
+  }).
+  catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+  });
